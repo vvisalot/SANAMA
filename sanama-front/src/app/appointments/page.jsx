61016e074a82f1c351a28694e87b016cbcd0e6c3
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-31 10:56:17 -0500

    .

--- sanama-front/src/app/appointments/page.jsx
+++ sanama-front/src/app/appointments/page.jsx
@@ -6,7 +6,6 @@ const AppointmentPage = () => {
   return (
     <section className="p-10">
       <h1 className="font-bold text-blue-500 text-6xl pb-8">Citas</h1>
-      <SearchAndAddBar linkHref="appointments/createAppointment" />
       <AppointmentTable />
     </section>
   );
