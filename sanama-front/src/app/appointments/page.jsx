0c76c157b55a387066d7060f3b675e3fdd498512
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-30 19:49:08 -0500

    .

--- sanama-front/src/app/appointments/page.jsx
+++ sanama-front/src/app/appointments/page.jsx
@@ -1,61 +1,45 @@
 //Listado de citas
-"use client"
+"use client";
 
-import SearchBar from "@/components/bars/SearchBar"
-import { useEffect, useState } from "react"
-import AppointmentTable from "./AppointmentTable"
-import { appointmentService } from "@/services/appointmentService"
-import Link from "next/link"
-import { parseAppointmentTable } from "@/util/appointmentParser"
+import SearchAndAddBar from "@/components/bars/SearchAndAddBar";
+import { useEffect, useState } from "react";
+import AppointmentTable from "./AppointmentTable";
+import { appointmentService } from "@/services/appointmentService";
+import { parseAppointmentTable } from "@/util/appointmentParser";
 
 const AppointmentPage = () => {
-    // const [filtro, setFiltro] = useState("")
-    const [appointmentTable, setAppointmentTable] = useState([])
+  // const [filtro, setFiltro] = useState("")
+  const [appointmentTable, setAppointmentTable] = useState([]);
 
-    const fetchData = async () => {
-        try {
-            const data = await appointmentService.listar()
-            const tableData = parseAppointmentTable(data)
-            setAppointmentTable(tableData)
-            console.log(data)
-        } catch (error) {
-            console.log("No se pudo obtener la lista de las citas")
-        }
+  const fetchData = async () => {
+    try {
+      const data = await appointmentService.listar();
+      const tableData = parseAppointmentTable(data);
+      setAppointmentTable(tableData);
+      console.log(data);
+    } catch (error) {
+      console.log("No se pudo obtener la lista de las citas");
     }
+  };
 
-    useEffect(() => {
-        fetchData()
-    }, [])
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const elements = e.target.elements
-        const filtro = elements.namedItem("search-bar-cita").value
-        fetchData(filtro)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const elements = e.target.elements;
+    const filtro = elements.namedItem("search-bar-cita").value;
+    fetchData(filtro);
+  };
 
-    return (
-        <>
-            <section className="flex items-center">
-                <h1 className="font-bold text-blue-500 text-6xl p-12" >Citas</h1>
-                <Link href='appointments/createAppointment'>
-                    <button className=" text-white bg-blue-700 hover:bg-blue-800  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2">
-                        Crear cita
-                    </button>
-                </Link>
-            </section>
+  return (
+    <section className="p-10">
+      <h1 className="font-bold text-blue-500 text-6xl pb-8">Citas</h1>
+      <SearchAndAddBar linkHref="appointments/createAppointment" />
+      <AppointmentTable data={appointmentTable}></AppointmentTable>
+    </section>
+  );
+};
 
-
-            <form className="flex pl-8 pr-10 pb-4" onSubmit={handleSubmit}>
-                <SearchBar name={"search-bar-cita"} width={"w-full"} />
-            </form>
-
-
-            <section className="pl-12 pr-14">
-                <AppointmentTable data={appointmentTable} ></AppointmentTable>
-            </section>
-        </>
-    )
-}
-
-export default AppointmentPage
+export default AppointmentPage;
---
+++ sanama-front/src/components/bars/SearchAndAddBar.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import Link from "next/link";
+
+const SearchAndAddBar = ({ searchTerm, onSearchChange, linkHref }) => (
+  <div className="my-2 p-4 bg-white shadow-md">
+    <div className="flex items-center space-x-4">
+      {/* Search Input */}
+      <div className="flex-1">
+        <div className="relative">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg
+              className="w-4 h-4 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 6h13M8 12h13m-7 6h7"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            className="block w-full p-2 pl-8 text-sm text-gray-900 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Buscar por nombre, email, especialidad..."
+            value={searchTerm}
+            onChange={onSearchChange}
+          />
+        </div>
+      </div>
+
+      {/* Search Button */}
+      <div className="flex-1">
+        <button className="w-full px-4 py-2 text-sm text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500">
+          Buscar
+        </button>
+      </div>
+
+      {/* Add Button */}
+      <div className="flex-1">
+        <Link href={linkHref}>
+          <href className="block w-full px-4 py-2 text-sm text-white text-center bg-green-600 rounded-lg hover:bg-green-700 focus:ring-2 focus:ring-green-500">
+            Nueva Cita
+          </href>
+        </Link>
+      </div>
+    </div>
+  </div>
+);
+
+export default SearchAndAddBar;
