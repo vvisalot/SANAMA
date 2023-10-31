b955545c8d00ce7498948ce1b609827c7332c04f
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-31 12:25:01 -0500

    use of memo

--- sanama-front/src/components/table/Table.jsx
+++ sanama-front/src/components/table/Table.jsx
@@ -1,15 +1,9 @@
+import React, { memo } from "react";
 import TableBody from "./TableBody";
 import TableHeader from "./TableHeader";
 
-const Table = ({
-  columns,
-  data,
-  url,
-  optionsText,
-  requestSort,
-  sortConfig,
-}) => {
-  return (
+const Table = memo(
+  ({ columns, data, url, optionsText, requestSort, sortConfig }) => (
     <table className="w-full">
       <TableHeader
         columns={columns}
@@ -23,7 +17,7 @@ const Table = ({
         optionsText={optionsText}
       />
     </table>
-  );
-};
+  )
+);
 
 export default Table;
