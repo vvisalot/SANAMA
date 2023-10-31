3686fa73b60151877c83b338bd0f1f5f5157bca6
zaicket <vargas.pedro@pucp.edu.pe>
2023-10-31 16:39:36 -0500

    cambios CALENDARIO registrarHorarioMedico y JSON

--- sanama-back/src/main/java/com/minsa/sanama/controller/rrhh/HorarioAtencionController.java
+++ sanama-back/src/main/java/com/minsa/sanama/controller/rrhh/HorarioAtencionController.java
@@ -1,6 +1,8 @@
 package com.minsa.sanama.controller.rrhh;
 
+import com.minsa.sanama.model.rrhh.TurnoAtencion;
 import com.minsa.sanama.services.rrhh.HorarioAtencionService;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rrhh")
@@ -24,16 +28,37 @@ public class HorarioAtencionController {
     public int registrarHorarioMedico(@RequestBody String pv_datos) {
         int idHorario = -1;
         try {
+            System.out.println(pv_datos);
             JSONObject job = (JSONObject) new JSONParser().parse(pv_datos);
             int pn_id_medico = Integer.parseInt(job.get("pn_id_medico").toString());
-            LocalTime pt_hora_inicio = LocalTime.parse(job.get("pt_hora_inicio").toString(),
+            String pd_fecha_inicio = job.get("pd_fecha_inicio").toString();
+            String pd_fecha_fin = job.get("pd_fecha_fin").toString();
+
+            List<TurnoAtencion> turnosAtencion = new ArrayList<>();
+            JSONArray arregloHorarios = (JSONArray) job.get("arregloHorarios");
+
+            for (Object horarioObject : arregloHorarios) {
+                JSONObject horario = (JSONObject) horarioObject;
+                String horaInicio = horario.get("horaInicio").toString();
+                String horaFin = horario.get("horaFin").toString();
+                String fecha = horario.get("fecha").toString();
+
+                // Crear objetos TurnoAtencion y agregarlos a la lista
+                TurnoAtencion turno = new TurnoAtencion();
+                turno.setHoraInicio(LocalTime.parse(horaInicio));
+                turno.setHoraFin(LocalTime.parse(horaFin));
+                turno.setFecha(LocalDate.parse(fecha));
+                System.out.println(turno.getHoraInicio().toString()+" "+turno.getFecha().toString());
+                turnosAtencion.add(turno);
+            }
+
+            /*LocalTime pt_hora_inicio = LocalTime.parse(job.get("pt_hora_inicio").toString(),
                     DateTimeFormatter.ofPattern("HH:mm"));
             LocalTime pt_hora_fin = LocalTime.parse(job.get("pt_hora_fin").toString(),
                     DateTimeFormatter.ofPattern("HH:mm"));
-            LocalDate pd_fecha = LocalDate.parse(job.get("pd_fecha").toString());
-            System.out.println(pv_datos);
-            idHorario = horarioAtencionService.registrarHorarioMedico(pn_id_medico, pt_hora_inicio, pt_hora_fin,
-                    pd_fecha);
+            LocalDate pd_fecha = LocalDate.parse(job.get("pd_fecha").toString());*/
+
+            idHorario = horarioAtencionService.registrarHorarioMedico(pn_id_medico, pd_fecha_inicio, pd_fecha_fin, turnosAtencion);
 
         } catch (Exception ex) {
             // Manejo de excepciones aquí
