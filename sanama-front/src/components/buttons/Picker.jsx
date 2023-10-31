8379cfd730091ccc4c30ac4373ee12f3e47b15e7
Valeria Visalot <valeria.visalot@pucp.edu.pe>
2023-10-31 16:25:54 -0500

    OOPSIE

--- sanama-front/src/components/buttons/Picker.jsx
+++ sanama-front/src/components/buttons/Picker.jsx
@@ -24,7 +24,7 @@ const Picker = ({ name1, name2, text, option1, option2, value, setValue }) => {
                         onChange={handleOptionChange}
                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                     <label
-                        htmlFor="option1"
+                        htmlFor={name1}
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                         {option1}
                     </label>
@@ -39,7 +39,7 @@ const Picker = ({ name1, name2, text, option1, option2, value, setValue }) => {
                         onChange={handleOptionChange}
                         className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                     <label
-                        htmlFor="option2"
+                        htmlFor={name2}
                         className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                         {option2}
                     </label>
