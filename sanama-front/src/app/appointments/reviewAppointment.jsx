955233d122b363eb5bb8d521b8020c1f64769547
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-10-31 10:26:52 -0500

    .

--- sanama-front/src/app/appointments/reviewAppointment.jsx
+++ sanama-front/src/app/appointments/reviewAppointment.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import {
-  Box,
-  Typography,
-  Grid,
-  Button,
-  TextField,
-  Container,
-  Paper,
-  InputLabel,
-  useTheme,
-} from "@mui/material";
 import Link from "next/link";
-import useAppointmentInfo from "./path/to/your/useAppointmentInfo";
+import useUpdateAppointmentStatus from "@/hooks/useUpdateAppointmentStatus";
+import useAppointmentInfo from "@/hooks/useAppointmentInfo"; // Asegúrate de importar el hook correcto
 
 const patientFieldsConfig = [
   { name: "codigoSeguro", label: "N° DE SEGURO" },
@@ -26,133 +16,157 @@ const patientFieldsConfig = [
 ];
 
 const camposAtencion = [
-  { id: "numero-cita", label: "Número de cita", type: "text" },
-  { id: "fecha-atencion", label: "Fecha de atención", type: "date" },
-  { id: "hora-atencion", label: "Hora de atención", type: "time" },
-  { id: "medico-responsable", label: "Médico responsable", type: "text" },
-  { id: "especialidad", label: "Especialidad", type: "text" },
+  { id: "numero-cita", label: "CODIGO DE CITA", type: "text" },
+  { id: "fecha-atencion", label: "FECHA DE ATENCION", type: "date" },
+  { id: "hora-atencion", label: "HORA DE ATENCION", type: "time" },
+  { id: "medico-responsable", label: "MEDICO RESPONSABLE", type: "text" },
+  { id: "especialidad", label: "ESPECIALIDAD", type: "text" },
   { id: "dniAcompanhante", label: "DOCUMENTO ACOMPAÑANTE", type: "text" },
   { id: "nombreAcompanhante", label: "NOMBRES ACOMPAÑANTE", type: "text" },
-  { id: "estado", label: "Estado", type: "text" },
+  { id: "estado", label: "ESTADO", type: "text" },
 ];
 
-function reviewAppointment(appointmentData) {
-  const theme = useTheme();
-  const {
-    loading,
-    error,
-    isAppointmentRegistered,
-    confirmationMessage,
-    handleRegisterAppointment,
-  } = useAppointmentInfo();
+const getFullName = (doctor) => {
+  return doctor
+    ? `${doctor.sexo === "M" ? "Dr." : "Dra."} ${doctor.nombres} ${
+        doctor.apellidoPaterno
+      } ${doctor.apellidoMaterno}`
+    : "";
+};
+
+const getSpecialtyName = (doctor) => {
+  return doctor && doctor.especialidad ? doctor.especialidad.nombre : "";
+};
+
+const PatientInfo = ({ pacienteData }) => (
+  <div className="bg-white p-6 rounded-lg shadow-md mb-6">
+    <h2 className="text-2xl font-semibold mb-4">Información del paciente</h2>
+    <div className="grid grid-cols-3 gap-4">
+      {patientFieldsConfig.map((campo) => (
+        <div key={campo.name}>
+          <label
+            htmlFor={campo.name}
+            className="block text-sm font-medium text-gray-700"
+          >
+            {campo.label}
+          </label>
+          <input
+            type="text" // Todos los inputs son de tipo texto y readonly
+            id={campo.name}
+            name={campo.name}
+            className="mt-1 p-2 w-full border rounded-md"
+            defaultValue={pacienteData ? pacienteData[campo.name] : ""}
+            readOnly
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+const AppointmentInfo = ({
+  appointmentData,
+  nombreDoctor,
+  especialidadNombre,
+}) => (
+  <div className="bg-white p-6 rounded-lg shadow-md mb-6">
+    <h2 className="text-2xl font-semibold mb-4">Información de la atención</h2>
+    <div className="grid grid-cols-3 gap-4">
+      {camposAtencion.map((campo) => (
+        <div key={campo.id}>
+          <label
+            htmlFor={campo.id}
+            className="block text-sm font-medium text-gray-700"
+          >
+            {campo.label}
+          </label>
+          <input
+            type={campo.type}
+            id={campo.id}
+            name={campo.id}
+            className="mt-1 p-2 w-full border rounded-md"
+            defaultValue={getValue(
+              appointmentData,
+              campo.id,
+              nombreDoctor,
+              especialidadNombre
+            )}
+            readOnly
+          />
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+const getValue = (appointmentData, id, nombreDoctor, especialidadNombre) => {
+  switch (id) {
+    case "fecha-atencion":
+      return appointmentData.selectedDate;
+    case "hora-atencion":
+      return appointmentData.selectedHour;
+    case "medico-responsable":
+      return nombreDoctor;
+    case "estado":
+      return "PENDIENTE";
+    case "especialidad":
+      return especialidadNombre;
+    default:
+      return appointmentData[id];
+  }
+};
+
+const ReviewAppointment = ({ appointmentData }) => {
+  const { loading, error, handleRegisterAppointment } = useAppointmentInfo();
 
   const pacienteData = appointmentData.selectedPatientData;
   const doctorResponsable = appointmentData.selectedDoctor;
-  const companionData = appointmentData.companionData;
-  const nombreDoctor = doctorResponsable
-    ? `${doctorResponsable.sexo === "M" ? "Dr." : "Dra."} ${
-        doctorResponsable.nombres
-      } ${doctorResponsable.apellidoPaterno} ${
-        doctorResponsable.apellidoMaterno
-      }`
-    : "";
-  const especialidadNombre =
-    doctorResponsable && doctorResponsable.especialidad
-      ? doctorResponsable.especialidad.nombre
-      : "";
-  const horaCita = appointmentData.selectedHour;
-  const fechaCita = appointmentData.selectedDate;
+  const nombreDoctor = getFullName(doctorResponsable);
+  const especialidadNombre = getSpecialtyName(doctorResponsable);
+
+  const { idCita, estado } = appointmentData;
+  const { updateAppointmentStatus } = useUpdateAppointmentStatus();
+
+  const handleAtenderCita = () => updateAppointmentStatus(idCita, 2);
+  const handleCancelarCita = () => updateAppointmentStatus(idCita, 3);
+  const handleReprogramarCita = () => {
+    /* ... */
+  }; // Implementa la lógica necesaria
 
   return (
     <div className="container mx-auto p-4">
-      <div className="bg-white p-6 rounded-lg shadow-md mb-6">
-        <h2 className="text-2xl font-semibold mb-4">
-          Información del paciente
-        </h2>
-        <div className="grid grid-cols-3 gap-4">
-          {patientFieldsConfig.map((campo) => (
-            <div key={campo.name}>
-              <label
-                htmlFor={campo.name}
-                className="block text-sm font-medium text-gray-700"
-              >
-                {campo.label}
-              </label>
-              <input
-                type={campo.type}
-                id={campo.name}
-                name={campo.name}
-                className="mt-1 p-2 w-full border rounded-md"
-                defaultValue={pacienteData ? pacienteData[campo.name] : ""}
-                readOnly
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="bg-white p-6 rounded-lg shadow-md mb-6">
-        <h2 className="text-2xl font-semibold mb-4">
-          Información de la atención
-        </h2>
-        <div className="grid grid-cols-3 gap-4">
-          {camposAtencion.map((campo) => (
-            <div key={campo.id}>
-              <label
-                htmlFor={campo.id}
-                className="block text-sm font-medium text-gray-700"
-              >
-                {campo.label}
-              </label>
-              <input
-                type={campo.type}
-                id={campo.id}
-                name={campo.id}
-                className="mt-1 p-2 w-full border rounded-md"
-                defaultValue={
-                  campo.id === "fecha-atencion"
-                    ? appointmentData.selectedDate
-                    : campo.id === "hora-atencion"
-                    ? appointmentData.selectedHour
-                    : campo.id === "medico-responsable"
-                    ? nombreDoctor
-                    : campo.id === "estado"
-                    ? "PENDIENTE"
-                    : campo.id === "especialidad"
-                    ? especialidadNombre
-                    : appointmentData[campo.id]
-                }
-                readOnly
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+      <PatientInfo pacienteData={pacienteData} />
+      <AppointmentInfo
+        appointmentData={appointmentData}
+        nombreDoctor={nombreDoctor}
+        especialidadNombre={especialidadNombre}
+      />
 
       <button
         className="bg-blue-500 text-white p-2 w-full rounded-md"
-        onClick={handleRegisterAppointment}
-        disabled={loading || isAppointmentRegistered}
+        onClick={handleAtenderCita}
+        disabled={loading || estado === "EN_CONSULTORIO"} // Asegúrate de deshabilitar el botón en el estado correspondiente
       >
         Atender Cita
       </button>
 
       {error && <p className="text-red-500 mt-2">{error}</p>}
-      {isAppointmentRegistered && (
+      {isStatusUpdated && (
         <p className="text-blue-500 mt-2">{confirmationMessage}</p>
       )}
 
       <button
         className="bg-blue-500 text-white p-2 w-full rounded-md mt-2"
-        disabled={loading || isAppointmentRegistered}
+        onClick={handleReprogramarCita}
+        disabled={loading}
       >
         Reprogramar Cita
       </button>
 
       <button
         className="bg-red-500 text-white p-2 w-full rounded-md mt-2"
-        onClick={handleRegisterAppointment}
-        disabled={loading || isAppointmentRegistered}
+        onClick={handleCancelarCita}
+        disabled={loading || estado === "CANCELADA"} // Asegúrate de deshabilitar el botón en el estado correspondiente
       >
         Cancelar
       </button>
@@ -164,6 +178,6 @@ function reviewAppointment(appointmentData) {
       </Link>
     </div>
   );
-}
+};
 
-export default reviewAppointment;
+export default ReviewAppointment;
