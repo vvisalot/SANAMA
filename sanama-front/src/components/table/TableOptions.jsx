d8265e025cc71edc75349b685c61d4a4995170cb
ANDRESR4444 <130945498+ANDRESR4444@users.noreply.github.com>
2023-11-01 01:49:11 -0500

    ahora si triaje final final final ya no lo quiero ver hasta fin de ciclo xd

--- sanama-front/src/components/table/TableOptions.jsx
+++ sanama-front/src/components/table/TableOptions.jsx
@@ -3,25 +3,42 @@
 //  Prop url: "/doctors/profile"
 // Patient url: "/patients/profile"
 
-import Link from 'next/link';  // Asumiendo que estás usando Next.js para el enlace.
+import Link from 'next/link';  
 import 'font-awesome/css/font-awesome.min.css';
 
-const TableOptions = ({ url, id, text, iconName }) => {
+const TableOptions = ({ url, id, text, iconName, estado }) => {
+  console.log("Estado recibido:", estado);
+  if (estado === "3" || estado === "Cancelada") {
+    return (
+      <td
+        scope="row"
+        className="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white cursor-not-allowed opacity-50"
+      >
+        <span className={`text-blue-700 hover:text-white border 
+            border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800`}>
+          <i className={`${iconName}`}></i>
+          {text}
+        </span>
+      </td>
+    );
+  }
+
   return (
     <td
       scope="row"
-      className="px-6 py-4  text-gray-900 whitespace-nowrap dark:text-white"
+      className="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
     >
       <Link
         href={`${url}/${id}`}
-        className="text-blue-700 hover:text-white border 
-            border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-      >
+        className={`text-blue-700 hover:text-white border 
+            border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800`}>
         <i className={`${iconName}`}></i>
         {text}
       </Link>
     </td>
   );
 };
+
+
 
 export default TableOptions;
