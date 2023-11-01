32c50de6904538697af47d65bd35ce3f0478260a
MauArenales <arenales.mauricio24@gmail.com>
2023-11-01 09:49:10 -0500

    listar disponibilidad de médico más rápido

---
+++ sanama-front/src/app/doctors/profile/[idDoctor]/Mensaje.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function Mensaje({ text }) {
+  const [message, setMessage] = useState("");
+
+  const handleMouseEnter = () => {
+    setMessage(text);
+  };
+
+  const handleMouseLeave = () => {
+    setMessage("");
+  };
+
+  return (
+    // <div className="relative cursor-pointer" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    //   {message && (
+    //     <div className="absolute top-0 -mt-10 bg-gray-200 border border-gray-300 rounded p-2 text-center">
+    //       {message}
+    //     </div>
+    //   )}
+    //   <span className="text-4xl">!</span>
+    // </div>
+    <div className="cursor-pointer inline-block relative" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <div className="mr-2 relative">
+        <div className="w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center relative">
+          <span className="text-white text-2xl">!</span>
+          {message && (
+            <div className="bg-blue-500 text-white text-sm px-2 py-1 rounded absolute top-0 left-10 z-50">
+              {message}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Mensaje;
