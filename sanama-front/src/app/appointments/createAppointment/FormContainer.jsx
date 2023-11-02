35c542af9a31463fb3494329b1230ffc6b9d624f
Valeria Visalot <valeria.visalot@pucp.edu.pe>
2023-11-02 02:45:15 -0500

    SE LLAMA PERO NO SE LLAMA ESA ES LA CUESTION

--- sanama-front/src/app/appointments/createAppointment/FormContainer.jsx
+++ sanama-front/src/app/appointments/createAppointment/FormContainer.jsx
@@ -4,6 +4,8 @@ import PatientForm from "./PatientForm"
 import usePatientForm from "@/hooks/usePatientForm"
 import { useEffect, useState } from 'react'
 import AppointementForm from "./AppointementForm"
+import { patientService } from "@/services/patientService"
+import { appointmentService } from "@/services/appointmentService"
 
 // Para acceder a los elementos
 // elements.namedItem("first_last_name").value
@@ -31,31 +33,120 @@ const FormContainer = () => {
         setTriageRequirement,
     } = useAppointmentForm()
 
-    const [formComplete, setFormComplete] = useState(false)
+
+    const registrarPaciente = async () => {
+        try {
+            const response = await patientService.registrarPaciente({
+                nombres: patientForm.nombres,
+                apellidoPaterno: patientForm.apellidoPaterno,
+                apellidoMaterno: patientForm.apellidoMaterno,
+                dni: patientForm.dni,
+                fechaNacimiento: fechaNacimiento,
+                sexo: sexo,
+                telefono: patientForm.telefono,
+                estado: 1,
+                codigoSeguro: patientForm.codigoSeguro,
+                tipoSeguro: patientForm.tipoSeguro,
+                direccion: patientForm.direccion,
+            })
+
+        } catch (error) {
+            console.log(response)
+        }
+    }
+
+    const registrarCita = async (params) => {
+        try {
+            const response = await appointmentService.registrarCita({
+                paciente: {
+                    idPersona: patientId
+                },
+                medico: {
+                    idPersona: doctorId
+                },
+                horaCita: appointmentData.selectedHour,
+                fechaCita: appointmentData.selectedDate,
+                tieneAcompanhante: legalResponsibilityForm.tieneAcompañante,
+                nombreAcompanhante: legalResponsibilityForm.nombres + ' ' + legalResponsibilityForm.apellidoPaterno + ' ' + legalResponsibilityForm.apellidoMaterno,
+                dniAcompanhante: legalResponsibilityForm.dni,
+                parentezco: companionData.relationship,
+                requiereTriaje: triageRequirement.requiereTriaje ? 1 : 0,
+            })
+        } catch (error) {
+            console.log(response)
+        }
+    }
 
     const handleSubmit = async (event) => {
-
-        //Evitar que el boton me rompa la pagina
         event.preventDefault()
-        //Recuperar datos del formulario
-        // console.log(patientForm)
 
-        //llamar al servicio
+        if (!patientId) {
+            try {
+                const patientData = {
+                    nombres: patientForm.nombres,
+                    apellidoPaterno: patientForm.apellidoPaterno,
+                    apellidoMaterno: patientForm.apellidoMaterno,
+                    dni: patientForm.dni,
+                    fechaNacimiento: fechaNacimiento,
+                    sexo: sexo,
+                    telefono: patientForm.telefono,
+                    estado: 1,
+                    codigoSeguro: patientForm.codigoSeguro,
+                    tipoSeguro: patientForm.tipoSeguro,
+                    direccion: patientForm.direccion,
+                }
 
-        //const response = await patientService.registrarPaciente(patientForm)
-        // const response = await citasService.citar({
-        //     paciente: { idPersona: pacienteData.idPersona },
-        //     medico: { idPersona: doctorResponsable.idPersona },
-        //     horaCita: appointmentData.selectedHour,
-        //     fechaCita: appointmentData.selectedDate,
-        //     tieneAcompanhante: companionData ? true : false,
-        //     nombreAcompanhante: companionData.nombres,
-        //     dniAcompanhante: companionData.documentoIdentidad,
-        //     parentezco: companionData.relationship,
-        //     requiereTriaje: selectedTriage ? 1 : 0,
-        //     estado: 1,
-        // })
+                const patientResponse = await patientService.registrarPaciente(patientFormData)
+
+                if (patientResponse && patientResponse.idPersona) {
+                    const newPatientId = patientResponse.idPersona
+                    setPatientId(newPatientId)
+
+                    const appointmentFormData = {
+                        idPaciente: newPatientId, // Cambia idPaciente por el ID del paciente recién registrado
+                        idMedico: doctorId,
+                        horaCita: schedule.selectedHour, // Ajusta el campo según tu formulario de cita
+                        fechaCita: schedule.selectedDate, // Ajusta el campo según tu formulario de cita
+                        tieneAcompanhante: legalResponsibilityForm.tieneAcompañante,
+                        nombreAcompanhante: `${legalResponsibilityForm.nombres} ${legalResponsibilityForm.apellidoPaterno} ${legalResponsibilityForm.apellidoMaterno}`,
+                        dniAcompanhante: legalResponsibilityForm.dni,
+                        parentezco: companionData.relationship, // Ajusta el campo según tu formulario de cita
+                        requiereTriaje: triageRequirement.requiereTriaje ? 1 : 0,
+                    }
+
+                    await appointmentService.registrarCita(appointmentFormData)
+                    console.log('Paciente registrado y cita registrada')
+
+                } else {
+                    console.log('Error al registrar paciente y cita')
+                }
+            } catch (error) {
+                console.log('Error en la respuesta del servidor para registrar un paciente y cita')
+            }
+        } else {
+            try {
+                const appointmentFormData = {
+                    idPaciente: patientId,
+                    idMedico: doctorId,
+                    horaCita: schedule.hora,
+                    fechaCita: schedule.fecha,
+                    tieneAcompanhante: legalResponsibilityForm.tieneAcompañante === 'Si' ? true : false,
+                    nombreAcompanhante: `${legalResponsibilityForm.nombres} ${legalResponsibilityForm.apellidoPaterno} ${legalResponsibilityForm.apellidoMaterno}`,
+                    dniAcompanhante: legalResponsibilityForm.dni,
+                    parentezco: legalResponsibilityForm.parentesco,
+                    requiereTriaje: triageRequirement.requiereTriaje ? 1 : 0,
+                }
+
+                await appointmentService.registrarCita(appointmentFormData)
+                console.log('YUPI')
+                console.log(appointmentFormData)
+            } catch (error) {
+                console.log('Error al registrar cita para un paciente existente')
+            }
+        }
     }
+
+
 
     useEffect(() => {
         // console.log(patientForm)
@@ -67,10 +158,15 @@ const FormContainer = () => {
 
     }, [patientForm, fechaNacimiento, sexo, legalResponsibilityForm, triageRequirement, schedule])
 
+    const [patientFormComplete, setPatientFormComplete] = useState(false)
+    const [appointmentFormComplete, setAppointmentFormComplete] = useState(false)
+    const [allFormComplete, setAllFormComplete] = useState(false)
+
+
     return (
         <form onSubmit={handleSubmit} className="p-10 w-full" >
             <PatientForm
-                setFormComplete={setFormComplete}
+                setFormComplete={setPatientFormComplete}
                 setPatientId={setPatientId}
                 patientForm={patientForm}
                 setPatientForm={setPatientForm}
@@ -81,27 +177,22 @@ const FormContainer = () => {
 
             <hr className="bg-gray-600 mt-20" />
 
-            {formComplete &&
+            {patientFormComplete &&
                 <>
                     <AppointementForm
+                        setFormComplete={setAppointmentFormComplete}
                         legalResponsibilityForm={legalResponsibilityForm}
                         setLegalResponsibilityForm={setLegalResponsibilityForm}
-                        doctorId={doctorId}
                         setDoctorId={setDoctorId}
                         schedule={schedule}
                         setSchedule={setSchedule}
                         triageRequirement={triageRequirement}
                         setTriageRequirement={setTriageRequirement}
+                        allFormComplete={allFormComplete}
+                        setAllFormComplete={setAllFormComplete}
                     />
-                    <div className="flex flex-row-reverse">
-                        <button type="submit" className=" m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
-                font-medium rounded-lg text-l w-full sm:w-auto px-5 py-3 text-center">
-                            Registrar cita
-                        </button>
-                    </div>
                 </>
             }
-
         </form >
     )
 }
