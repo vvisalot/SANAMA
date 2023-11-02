4ffcf374751dc0c27468bb15301a0067883e3c98
EnriqueIgnacio1011 <82422553+EnriqueIgnacio1011@users.noreply.github.com>
2023-11-02 01:44:05 -0500

    Orden de Laboratorio

--- sanama-back/src/main/java/com/minsa/sanama/services/laboratorio/OrdenLaboratorioService.java
+++ sanama-back/src/main/java/com/minsa/sanama/services/laboratorio/OrdenLaboratorioService.java
@@ -1,16 +1,21 @@
 package com.minsa.sanama.services.laboratorio;
 
+import com.minsa.sanama.model.laboratorio.ExamenMedico;
 import com.minsa.sanama.model.laboratorio.OrdenLaboratorio;
+import com.minsa.sanama.repository.laboratorio.ExamenMedicoRepository;
 import com.minsa.sanama.repository.laboratorio.OrdenLaboratorioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class OrdenLaboratorioService {
     @Autowired
     OrdenLaboratorioRepository ordenLaboratorioRepository;
+    @Autowired
+    ExamenMedicoRepository examenMedicoRepository;
 
     public List<OrdenLaboratorio> listarOrdenLaboratorioFiltro(String pv_filtro, String pd_fecha_inicio, String pd_fecha_fin) {
         List<OrdenLaboratorio> lordenes;
@@ -19,14 +24,26 @@ public class OrdenLaboratorioService {
     }
 
     public OrdenLaboratorio buscarOrdenLaboratorio(String pn_id_orden_laboratorio) {
-        List<OrdenLaboratorio> lordenes;
-        lordenes = ordenLaboratorioRepository.buscarOrdenLaboratorio(pn_id_orden_laboratorio);
-        if (!lordenes.isEmpty()) {
-            return lordenes.get(0);
-        } else {
-            // Manejar el caso en que la lista está vacía, por ejemplo, lanzar una excepción o devolver un valor predeterminado.
-            // Aquí un ejemplo de devolver null:
-            return null;
+        OrdenLaboratorio ordenLaboratorio;
+        List<ExamenMedico> lexamenes;
+        ordenLaboratorio = ordenLaboratorioRepository.buscarOrdenLaboratorio(pn_id_orden_laboratorio).get(0);
+        lexamenes = examenMedicoRepository.buscarExamenMedico(pn_id_orden_laboratorio);
+        ordenLaboratorio.setExamenMedico(lexamenes);
+
+        return ordenLaboratorio;
+    }
+
+    public int atenderOrdenLaboratorioV1(OrdenLaboratorio orden){
+        List<ExamenMedico> lexamenes=null;
+        int valido;
+        valido = ordenLaboratorioRepository.atenderOrdenLaboratorioV1(orden);
+        lexamenes = orden.getExamenMedico();
+        if(valido==1){
+            for (ExamenMedico examen : lexamenes) {
+                examenMedicoRepository.registrarExamenMedico(examen,orden.getIdOrdenLaboratorio());
+            }
+            return 1;
         }
+        return 0;
     }
 }
