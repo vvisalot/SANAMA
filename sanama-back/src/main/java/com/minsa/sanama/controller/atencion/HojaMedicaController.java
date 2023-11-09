75492452da4fec21d24314a8e5cff783e610c9c9
zaicket <vargas.pedro@pucp.edu.pe>
2023-11-09 12:14:25 -0500

    eliminaHojaMedica y POSTMAN

--- sanama-back/src/main/java/com/minsa/sanama/controller/atencion/HojaMedicaController.java
+++ sanama-back/src/main/java/com/minsa/sanama/controller/atencion/HojaMedicaController.java
@@ -3,6 +3,8 @@ package com.minsa.sanama.controller.atencion;
 import com.minsa.sanama.model.atencionmedica.HistorialClinico;
 import com.minsa.sanama.model.atencionmedica.HojaMedica;
 import com.minsa.sanama.services.atencion.HojaMedicaService;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +25,23 @@ public class HojaMedicaController {
         int n;
         hojaMedica = historialClinico.getHojasMedicas().get(0);
         n = hojaMedicaService.registrarHojaMedica(hojaMedica,historialClinico.getIdHistorialClinico());
+        return n;
+    }
+
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE},
+            value = "/post/eliminarHojaMedica")
+    @ResponseBody
+    public int eliminarHojaMedica(@RequestBody String pv_datos){
+        int n=0;
+        try{
+            JSONObject job = (JSONObject) new JSONParser().parse(pv_datos);
+            String pn_id_hoja_medica = job.get("pn_id_hoja_medica").toString();
+            n = hojaMedicaService.eliminarHojaMedica(pn_id_hoja_medica);
+        }catch(Exception ex){
+            // Manejo de excepciones aquí
+            ex.printStackTrace();
+        }
         return n;
     }
 }
