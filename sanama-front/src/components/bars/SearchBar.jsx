6224fa578c41eae673b2e76d86de3e6e7e95c8ce
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-09 20:20:43 -0500

    .

--- sanama-front/src/components/bars/SearchBar.jsx
+++ sanama-front/src/components/bars/SearchBar.jsx
@@ -1,24 +1,41 @@
-"use client"
+"use client";
 const SearchBar = ({ name, width, placeholderText }) => {
-    return (
-        <div className={`py-2 px-4 ${width} flex items-center`}>
-            <div className="relative w-full mr-2">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg>
-                </div>
-                <input
-                    name={name}
-                    id={name}
-                    type="search"
-                    className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-                    placeholder={placeholderText}
-                />
-            </div>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-4">Buscar</button>
+  return (
+    <div className={`py-2 ${width} flex items-center`}>
+      <div className="relative w-full mr-2">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
         </div>
-    )
-}
+        <input
+          name={name}
+          id={name}
+          type="search"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder={placeholderText}
+        />
+      </div>
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-4"
+      >
+        Buscar
+      </button>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
