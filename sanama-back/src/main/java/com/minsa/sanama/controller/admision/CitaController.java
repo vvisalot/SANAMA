7b1e2500a8b512eba4afacc15b31f4d03fab9d4b
zaicket <vargas.pedro@pucp.edu.pe>
2023-11-10 03:43:06 -0500

    listarCitasDeMedicoPorFiltro y POSTMAN

--- sanama-back/src/main/java/com/minsa/sanama/controller/admision/CitaController.java
+++ sanama-back/src/main/java/com/minsa/sanama/controller/admision/CitaController.java
@@ -188,4 +188,47 @@ public class CitaController {
         return citas;
     }
 
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE }, produces = {
+            MediaType.APPLICATION_JSON_VALUE }, value = "/post/listarCitasDeMedicoPorFiltro")
+    @ResponseBody
+    public List<CitaMedica> listarCitasDeMedicoPorFiltro(@RequestBody String pv_datos) {
+        List<CitaMedica> Lcita = null;
+        try {
+            JSONObject job = (JSONObject) new JSONParser().parse(pv_datos);
+            String pv_filtro = job.get("pv_filtro").toString();
+            String pn_id_medico = job.get("pn_id_medico").toString();
+            String pd_fecha_inicio;
+            String pd_fecha_fin;
+            String estado;
+            //String pn_estado;
+
+            if(job.get("pd_fecha_inicio") == null) pd_fecha_inicio=null;
+            else pd_fecha_inicio = job.get("pd_fecha_inicio").toString();
+
+            if(job.get("pd_fecha_fin") == null) pd_fecha_fin=null;
+            else pd_fecha_fin = job.get("pd_fecha_fin").toString();
+
+            boolean flag=true;
+            List<String> estados = new ArrayList<>();
+            JSONArray arregloEstados = (JSONArray) job.get("arregloEstados");
+            if (arregloEstados != null){
+                for (Object estadoObjetc : arregloEstados) {
+                    JSONObject pn_estado = (JSONObject) estadoObjetc;
+                    if(pn_estado.get("estado") == null) estado=null;
+                    else estado = pn_estado.get("estado").toString();
+                    flag=false;
+                    estados.add(estado);
+                }
+            }
+            if(flag)estados.add(null);
+
+            // Llama al servicio para listar citas por filtros
+            Lcita = citaService.listarCitasDeMedicoxFiltro(pn_id_medico, pv_filtro, pd_fecha_inicio, pd_fecha_fin, estados);
+        } catch (Exception ex) {
+            // Manejo de excepciones aquí
+            ex.printStackTrace();
+        }
+        return Lcita;
+    }
+
 }
