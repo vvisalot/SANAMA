e28ef85bb1e4b2f2d66edfc6cb01e681f1f5ce83
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-10 05:30:32 -0500

    '

--- sanama-front/src/app/page.jsx
+++ sanama-front/src/app/page.jsx
@@ -5,8 +5,8 @@ import React from "react";
 import NewsFeed from "@/components/NewsFeed";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div className=" font-bold text-blue-500">Pagina de inicio</div>
+    <main className="flex flex-col items-center justify-between p-10">
+      <div className="font-bold text-blue-500">Noticias Medicas</div>
       <NewsFeed />
     </main>
   );
--- sanama-front/src/components/NewsItem.jsx
+++ sanama-front/src/components/NewsItem.jsx
@@ -1,14 +1,39 @@
 import React from "react";
 
 const NewsItem = ({ news }) => {
+  // Función para formatear la fecha
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
+
+  // Imagen predeterminada en caso de que news.urlToImage sea nulo
+  const defaultImage = "path_to_your_default_image.jpg"; // Reemplaza con tu imagen predeterminada
+
   return (
-    <div className="news-item">
-      <h3>{news.title}</h3>
-      <img src={news.urlToImage} alt={news.title} />
-      <p>{news.description}</p>
-      <a href={news.url} target="_blank" rel="noopener noreferrer">
-        Read more
-      </a>
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden my-4">
+      <img
+        src={news.urlToImage || defaultImage}
+        alt={news.title}
+        className="w-full h-64 object-cover object-center"
+      />
+      <div className="p-4">
+        <h3 className="font-bold text-primary-dark-blue text-xl mb-2">
+          {news.title}
+        </h3>
+        <p className="text-gray-one mb-4">{formatDate(news.publishedAt)}</p>
+        <p className="text-metal mb-4">
+          {news.description ? news.description.substring(0, 100) + "..." : ""}
+        </p>
+        <a
+          href={news.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-secondary-orange hover:text-secondary-dark-orange font-semibold"
+        >
+          Read more
+        </a>
+      </div>
     </div>
   );
 };
