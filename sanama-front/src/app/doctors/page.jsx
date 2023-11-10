9087ec91133c9c943ca591dc416c99a5c10aaad3
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-09 23:35:47 -0500

    .

--- sanama-front/src/app/doctors/page.jsx
+++ sanama-front/src/app/doctors/page.jsx
@@ -1,58 +1,63 @@
-"use client"
-import DoctorTable from "./DoctorsTable"
-import { doctorService } from "@/services/doctorService"
-import { useEffect, useState } from "react"
-import { parseDoctorsTable } from "@/util/doctorParser"
-import Dropdown from "@/components/Dropdowns/Dropdown"
-import SearchBar from "@/components/bars/SearchBar"
-import Link from "next/link"
+"use client";
+import DoctorTable from "./DoctorsTable";
+import { doctorService } from "@/services/doctorService";
+import { useEffect, useState } from "react";
+import { parseDoctorsTable } from "@/util/doctorParser";
+import Dropdown from "@/components/Dropdowns/Dropdown";
+import SearchBar from "@/components/bars/SearchBar";
+import TitleWithIcon from "@/components/TitleWithIcon";
+import DoctorIcon from "@/components/icons/DoctorIcon";
+
 const DoctorsPage = () => {
-  const [doctorTable, setDoctorTable] = useState([])
-  const [specialties, setSpecialties] = useState([])
-  const [especialidadSeleccionada, setEspecialidadSeleccionada] = useState("Todas las especialidades")
+  const [doctorTable, setDoctorTable] = useState([]);
+  const [specialties, setSpecialties] = useState([]);
+  const [especialidadSeleccionada, setEspecialidadSeleccionada] = useState(
+    "Todas las especialidades"
+  );
   const fetchData = async (filtro, especialidad) => {
     try {
-      const data = await doctorService.buscarPorMedicoEspecialidad(filtro, especialidad)
-      const tableData = parseDoctorsTable(data)
+      const data = await doctorService.buscarPorMedicoEspecialidad(
+        filtro,
+        especialidad
+      );
+      const tableData = parseDoctorsTable(data);
       //console.log(tableData)
-      setDoctorTable(tableData)
+      setDoctorTable(tableData);
     } catch (error) {
-      console.log("No se pudo obtener los datos de los doctores")
+      console.log("No se pudo obtener los datos de los doctores");
     }
-  }
+  };
 
   const fetchSpecialty = async () => {
     try {
-      const data = await doctorService.listarEspecialidades()
-      setSpecialties(data)
+      const data = await doctorService.listarEspecialidades();
+      setSpecialties(data);
       //console.log(data)
     } catch (error) {
-      console.log("No se pudo obtener los datos de las especialidades")
+      console.log("No se pudo obtener los datos de las especialidades");
     }
-  }
-
+  };
 
   useEffect(() => {
-    fetchData("", "")
-    fetchSpecialty()
-  }, [])
+    fetchData("", "");
+    fetchSpecialty();
+  }, []);
 
   // useEffect(() => {
   //     fetchData("", especialidadSeleccionada)
   // }, [especialidadSeleccionada])
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const elements = e.target.elements
-    const dropdownValue = elements.namedItem("speciality-dropdown").value
-    const filtro = elements.namedItem("doctor-search").value
-    fetchData(filtro, dropdownValue)
-  }
-
+    e.preventDefault();
+    const elements = e.target.elements;
+    const dropdownValue = elements.namedItem("speciality-dropdown").value;
+    const filtro = elements.namedItem("doctor-search").value;
+    fetchData(filtro, dropdownValue);
+  };
 
   return (
-    <>
-      <h1 className="font-bold text-blue-500 text-6xl p-12" >Médicos</h1>
+    <section className="p-4 md:p-14">
+      <TitleWithIcon name={"Medicos"} Icon={DoctorIcon} />
       <form className="flex px-10 pb-4" onSubmit={handleSubmit}>
         <Dropdown
           data={specialties}
@@ -62,25 +67,28 @@ const DoctorsPage = () => {
           value={"nombre"}
           name={"speciality-dropdown"}
           width={"w-[400px]"}
-          handleChange={(event) => { setEspecialidadSeleccionada(event.target.value) }}
+          handleChange={(event) => {
+            setEspecialidadSeleccionada(event.target.value);
+          }}
         ></Dropdown>
         <SearchBar
           name={"doctor-search"}
           width={"w-full"}
           placeholderText={"Buscar por nombre o CMP"}
-          hrefBtnNuevo ={"/doctors/newDoctor"}>
-        </SearchBar>
-        
-
+          hrefBtnNuevo={"/doctors/newDoctor"}
+        ></SearchBar>
       </form>
-      <div style={{ marginBottom: '1rem', color: 'black' }} className="pl-12 pr-14">
+      <div
+        style={{ marginBottom: "1rem", color: "black" }}
+        className="pl-12 pr-14"
+      >
         Número de resultados: {doctorTable.length}
       </div>
       <section className="pl-12 pr-14">
         <DoctorTable data={doctorTable}></DoctorTable>
       </section>
-    </>
-  )
-}
+    </section>
+  );
+};
 
-export default DoctorsPage
+export default DoctorsPage;
--- sanama-front/src/app/triajes/page.jsx
+++ sanama-front/src/app/triajes/page.jsx
@@ -4,6 +4,8 @@ import TriajeTable from "./TriajeTable";
 import { parseTriajeTable } from "@/util/triajeParser";
 import SearchBar from "@/components/bars/SearchBar";
 import { triajeService } from "@/services/triajeService";
+import TitleWithIcon from "@/components/TitleWithIcon";
+import TriageIcon from "@/components/icons/TriageIcon";
 
 const TriajePage = () => {
   const [triajeTable, setTriajeTable] = useState([]);
@@ -14,31 +16,33 @@ const TriajePage = () => {
       const addHours = (dateStr, hoursToAdd) => {
         let date = new Date(dateStr);
         date.setHours(date.getHours() + hoursToAdd);
-        
+
         // Formatear de vuelta al formato original
-        let dd = String(date.getDate()).padStart(2, '0');
-        let mm = String(date.getMonth() + 1).padStart(2, '0'); // Enero es 0!
+        let dd = String(date.getDate()).padStart(2, "0");
+        let mm = String(date.getMonth() + 1).padStart(2, "0"); // Enero es 0!
         let yyyy = date.getFullYear();
-        let hh = String(date.getHours()).padStart(2, '0');
-        let min = String(date.getMinutes()).padStart(2, '0');
-        let ss = String(date.getSeconds()).padStart(2, '0');
-    
+        let hh = String(date.getHours()).padStart(2, "0");
+        let min = String(date.getMinutes()).padStart(2, "0");
+        let ss = String(date.getSeconds()).padStart(2, "0");
+
         return `${yyyy}-${mm}-${dd} ${hh}:${min}:${ss}`; // Esto asume que tu formato es "YYYY-MM-DD HH:mm:ss"
-    }
-    
-    const fechaDesdeToSend = fechaDesde ? addHours(fechaDesde, 15) : null;
-    const fechaHastaToSend = fechaHasta ? addHours(fechaHasta, 15) : null;
+      };
 
-    
+      const fechaDesdeToSend = fechaDesde ? addHours(fechaDesde, 15) : null;
+      const fechaHastaToSend = fechaHasta ? addHours(fechaHasta, 15) : null;
 
-        const data = await triajeService.listarTriajePorFiltro(filtro, fechaDesdeToSend, fechaHastaToSend)
-        console.log(data);
-        const tableData = parseTriajeTable(data);
-        setTriajeTable(tableData);
+      const data = await triajeService.listarTriajePorFiltro(
+        filtro,
+        fechaDesdeToSend,
+        fechaHastaToSend
+      );
+      console.log(data);
+      const tableData = parseTriajeTable(data);
+      setTriajeTable(tableData);
     } catch (error) {
-        console.log("No se pudo obtener los datos de los triajes");
+      console.log("No se pudo obtener los datos de los triajes");
     }
-    }
+  };
 
   useEffect(() => {
     fetchData("");
@@ -51,17 +55,14 @@ const TriajePage = () => {
     const fechaDesde = elements.namedItem("fechaDesde").value;
     const fechaHasta = elements.namedItem("fechaHasta").value;
     fetchData(filtro, fechaDesde, fechaHasta);
-};
-
+  };
 
   return (
-    <section className="p-10">
-      <h1 className="font-bold text-blue-500 text-6xl pb-8">
-        Triajes
-      </h1>
+    <section className="p-4 md:p-14">
+      <TitleWithIcon name={"Triaje"} Icon={TriageIcon} />
+
       <form className="w-full" onSubmit={handleSubmit}>
         <div className="flex justify-start items-center">
-          
           <div className="ml-4">
             <label htmlFor="fechaDesde" className="mr-2">
               Desde:
@@ -80,10 +81,12 @@ const TriajePage = () => {
             width={"flex-grow"}
             placeholderText="Buscar por Nombre o DNI"
           />
-          
         </div>
       </form>
-      <div style={{ marginBottom: "1rem", color: "black" }} className="pl-12 pr-14">
+      <div
+        style={{ marginBottom: "1rem", color: "black" }}
+        className="pl-12 pr-14"
+      >
         Número de resultados: {triajeTable.length}
       </div>
       <section className="pl-4 pr-2">
@@ -94,4 +97,3 @@ const TriajePage = () => {
 };
 
 export default TriajePage;
-
