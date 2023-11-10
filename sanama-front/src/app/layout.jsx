9e6cb6e5ac21b4be552eea6e31cffbb5797feaa8
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-09 22:17:24 -0500

    .

--- sanama-front/src/app/layout.jsx
+++ sanama-front/src/app/layout.jsx
@@ -1,33 +1,41 @@
-"use client"
-import { useState, useEffect } from 'react'
-import { Inter } from 'next/font/google'
-import './globals.css'
+"use client";
+import { useState, useEffect } from "react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NextBreadcrumb from "@/components/NextBreadcrumb";
+import Sidebar from "@/components/sidebar/Sidebar";
 
-import Sidebar from '@/components/sidebar/Sidebar'
-
-const inter = Inter({ subsets: ['latin'] })
-
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const toggleSidebar = () => {
-    setIsSidebarOpen((prevIsSidebarOpen) => !prevIsSidebarOpen)
-  }
-
+    setIsSidebarOpen((prevIsSidebarOpen) => !prevIsSidebarOpen);
+  };
 
   return (
     <html lang="en">
       <head>
         <title>SANAMA</title>
       </head>
-      <body className={`${inter.className} flex  transition-all ease-linear duration-300`}>
+      <body
+        className={`${inter.className} flex  transition-all ease-linear duration-300`}
+      >
         <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-        <main className="min-h-screen box-border w-full border-[40px] border-slate-300 bg-slate-200">
-          {children}
 
+        <main className="min-h-screen box-border w-full border-[40px] border-slate-300 bg-slate-200">
+          <NextBreadcrumb
+            homeElement={"Home"}
+            separator={<span> | </span>}
+            activeClasses="text-gray-900 font-bold"
+            containerClasses="flex py-5 bg-gradient-to-r from-black-600 to-blue-600"
+            listClasses="text-blue-500 hover:text-blue-700"
+            capitalizeLinks
+          />
+          {children}
         </main>
       </body>
-    </html >
-  )
+    </html>
+  );
 }
