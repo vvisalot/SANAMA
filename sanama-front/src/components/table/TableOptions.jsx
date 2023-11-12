d8fa2deb532bf2b00eac925e895702c85452ebed
MauArenales <arenales.mauricio24@gmail.com>
2023-11-12 03:03:49 -0500

    redirigiendo a cita de paciente desde perfil doc

--- sanama-front/src/components/table/TableOptions.jsx
+++ sanama-front/src/components/table/TableOptions.jsx
@@ -26,7 +26,7 @@ const TableOptions = ({ url, id, text, iconName, estado }) => {
       className="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white"
     >
       <Link
-        href={`${url}/${id}`}
+        href={`/${url}/${id}`}
         className={`text-blue-700 hover:text-white border 
             border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800`}
       >
