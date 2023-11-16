5de734f3d40644f423a999a79575f1ef72163eac
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-16 13:15:34 -0500

    .

--- sanama-front/src/components/evaluations/ExplorationTab.jsx
+++ sanama-front/src/components/evaluations/ExplorationTab.jsx
@@ -43,7 +43,7 @@ const ExplorationTab = ({ formData, handleInputChange }) => {
         type="checkbox"
         checked={visibleSections[section]}
         onChange={() => toggleSectionVisibility(section)}
-        className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        className="w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring dark:bg-gray-700 dark:border-gray-600"
       />
       <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
         {label}
