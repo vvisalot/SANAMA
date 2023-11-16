f46c49e49d9ede6365f5560a2d462a90490d81e0
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-16 15:43:07 -0500

    anti estupidez

--- sanama-front/src/components/evaluations/TreatmentTab.jsx
+++ sanama-front/src/components/evaluations/TreatmentTab.jsx
@@ -22,7 +22,7 @@ const TratamientoYDecisionCita = () => {
   const addRecetaMedicaField = () => {
     const lastReceta =
       tratamientoData.recetasMedicas[tratamientoData.recetasMedicas.length - 1];
-    if (lastReceta.medicamento && lastReceta.indicaciones) {
+    if (!lastReceta || (lastReceta.medicamento && lastReceta.indicaciones)) {
       setTratamientoData((prevState) => ({
         ...prevState,
         recetasMedicas: [
@@ -51,7 +51,7 @@ const TratamientoYDecisionCita = () => {
 
   return (
     <div className="p-8">
-      <h4 className="text-lg font-bold text-gray-700 mb-2">Receta Medica</h4>
+      <h4 className="text-lg font-bold text-gray-700 mb-2">Receta Médica</h4>
 
       <div className="col-span-2">
         <label className="block text-sm font-medium text-gray-700">
@@ -62,21 +62,40 @@ const TratamientoYDecisionCita = () => {
             <input
               type="text"
               value={receta.medicamento}
+              readOnly={index !== tratamientoData.recetasMedicas.length - 1}
               onChange={(e) =>
                 handleArrayChange(index, "medicamento", e.target.value)
               }
-              className="mt-1 p-2 w-3/6 border-gray-300 rounded-md"
+              className={`mt-1 p-2 w-3/6 border-gray-300 rounded-md ${
+                index !== tratamientoData.recetasMedicas.length - 1
+                  ? "bg-gray-200"
+                  : ""
+              }`}
               placeholder={`Medicamento ${index + 1}`}
             />
             <input
               type="text"
               value={receta.indicaciones}
+              readOnly={index !== tratamientoData.recetasMedicas.length - 1}
               onChange={(e) =>
                 handleArrayChange(index, "indicaciones", e.target.value)
               }
-              className="mt-1 p-2 w-full border-gray-300 rounded-md"
+              className={`mt-1 p-2 w-full border-gray-300 rounded-md ${
+                index !== tratamientoData.recetasMedicas.length - 1
+                  ? "bg-gray-200"
+                  : ""
+              }`}
               placeholder={`Indicaciones ${index + 1}`}
             />
+            {index !== tratamientoData.recetasMedicas.length - 1 && (
+              <button
+                type="button"
+                onClick={() => removeRecetaMedicaField(index)}
+                className="bg-blue-500 text-white p-2 rounded-md"
+              >
+                Edit
+              </button>
+            )}
             <button
               type="button"
               onClick={() => removeRecetaMedicaField(index)}
