101ff87db3c5219c763c94f685d9d85abc1a84a7
MauArenales <arenales.mauricio24@gmail.com>
2023-11-17 18:43:56 -0500

    modificación insana del perfil del médico

---
+++ sanama-front/src/app/doctors/profile/[idDoctor]/DoctorActions.jsx
@@ -0,0 +1,24 @@
+import ActionButton from "@/components/buttons/ActionButton";
+import Link from "next/link";
+
+const DoctorActions = ({ id }) => {
+  return (
+    <section className="my-10 rounded-2xl h-full grid grid-rows-2 items-center bg-white shadow">
+      <ActionButton
+        url={`/doctors/profile/${id}/disponibilidad`}
+        color={"bg-orange-400"}
+        shadow={"shadow-orange-200"}
+        name={"Registrar disponibilidad"}
+      />
+
+      <ActionButton
+        url={`/doctors/profile/${id}/misCitas`}
+        color={"bg-red-400"}
+        shadow={"shadow-red-200"}
+        name={"Mis citas"}
+      />
+    </section>
+  );
+};
+
+export default DoctorActions;
