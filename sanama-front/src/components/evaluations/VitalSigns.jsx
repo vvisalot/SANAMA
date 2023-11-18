ae7ed7ed1da74fd047b692b4bbf0964ba3e9469e
Gonzalo <69012359+Duvet05@users.noreply.github.com>
2023-11-18 17:50:28 -0500

    corregido

--- sanama-front/src/components/evaluations/VitalSigns.jsx
+++ sanama-front/src/components/evaluations/VitalSigns.jsx
@@ -9,14 +9,26 @@ const VitalSigns = ({ defaultTriaje, setMedicalRecordData }) => {
     const regex = new RegExp(pattern);
     const isValid = regex.test(value);
 
-    setMedicalRecordData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+    setMedicalRecordData((prevState) => {
+      const sections = name.split(".");
+      if (sections.length === 3) {
+        const [_, section, key] = sections;
+        return {
+          ...prevState,
+          evaluacionMedica: {
+            [section]: {
+              ...prevState.evaluacionMedica[section],
+              [key]: value,
+            },
+          },
+        };
+      }
+      return prevState;
+    });
 
     setErrors((prevErrors) => ({
       ...prevErrors,
-      [name]: value ? !regex.test(value) : false,
+      [name]: value ? !isValid : false,
     }));
   };
 
@@ -24,35 +36,35 @@ const VitalSigns = ({ defaultTriaje, setMedicalRecordData }) => {
     {
       type: "number",
       label: "Temperatura (°C)",
-      name: "temperatura",
+      name: "evaluacionMedica.signosVitales.temperatura",
       value: defaultTriaje.temperatura,
       pattern: "^(3[6-9](\\.\\d)?|40(\\.0)?)$", // Asegúrate de escapar los caracteres especiales
     },
     {
       type: "number",
       label: "Frecuencia Cardiaca (FC)",
-      name: "frecuenciaCardiaca",
+      name: "evaluacionMedica.signosVitales.frecuenciaCardiaca",
       value: defaultTriaje.frecuenciaCardiaca,
       pattern: "^([6-9]\\d|100)$", // Valores entre 60 y 100
     },
     {
       type: "number",
       label: "Frecuencia Respiratoria (FR)",
-      name: "frecuenciaRespiratoria",
+      name: "evaluacionMedica.signosVitales.frecuenciaRespiratoria",
       value: defaultTriaje.frecuenciaRespiratoria,
       pattern: "^1[2-9]|20$", // Valores entre 12 y 20
     },
     {
       type: "text",
       label: "Presión Arterial (Sistolica / Diastolica)",
-      name: "presionArterial",
+      name: "evaluacionMedica.signosVitales.presionArterial",
       value: defaultTriaje.presionArterial,
       pattern: "^(1[0-2]\\d|130)\\/(6\\d|7[0-9]|80)$", // Formato como "120/80"
     },
     {
       type: "number",
       label: "Saturación de Oxígeno (%)",
-      name: "saturacionOxigeno",
+      name: "evaluacionMedica.signosVitales.saturacionOxigeno",
       value: defaultTriaje.saturacionOxigeno,
       pattern: "^(9[5-9]|100)$", // Valores entre 95 y 100
     },
