f38146d313fb5c50b4a7870cda793b03feefbb73
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-18 22:57:33 -0500

    .

--- sanama-front/src/components/appointments/create/ScheduleChip.jsx
+++ sanama-front/src/components/appointments/create/ScheduleChip.jsx
@@ -2,7 +2,7 @@ const ScheduleChip = ({ start, end, isSelected, onSelect }) => {
   return (
     <div
       onClick={onSelect}
-      className={`[word-wrap: break-word] my-[5px] mr-4 flex h-[32px] cursor-pointer items-center justify-between rounded-[16px] 
+      className={`[word-wrap: break-word] my-[5px] mr-4 h-[32px] cursor-pointer items-center justify-between rounded-[16px] 
             px-[12px] py-0 text-[13px] font-normal normal-case leading-loose shadow-none 
             transition-[opacity] duration-300 ease-linear hover:!shadow-none active:bg-[#cacfd1] 
             ${
