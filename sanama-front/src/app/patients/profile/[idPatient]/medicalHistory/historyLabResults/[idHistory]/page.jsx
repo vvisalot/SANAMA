2d4b9b4f3cdf04eb109da51f3348c14836fbeb56
Valeria Visalot <valeria.visalot@pucp.edu.pe>
2023-11-20 01:13:13 -0500

    inicio de pantalla resultados

--- sanama-front/src/app/patients/profile/[idPatient]/medicalHistory/historyLabResults/[idHistory]/page.jsx
+++ sanama-front/src/app/patients/profile/[idPatient]/medicalHistory/historyLabResults/[idHistory]/page.jsx
@@ -1,15 +1,40 @@
 "use client"
-const HistoryLabResults = ({ params }) => {
 
+import { useEffect } from "react"
+import LabResultsTable from "./labResultsTable"
+
+const HistoryLabResults = ({ params }) => {
+    const fetchData = async () => {
+        try {
+            const data = await patientService.buscarHistorialClinico(params.idPatient)
+            console.log(data.idHistorialClinico)
+        }
+        catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    useEffect(() => {
+        fetchData()
+    })
+
+    const options = {
+        text: "Ver archivo",
+        link: "",
+        icons: "/icons/eye.svg"
+    }
     return (
-        <div>
-            <h1>Resultados Laboratorio</h1>
+        <div className="px-80">
+            <h1 className="text-4xl font-bold mt-5 mb-10 text-primary-dusk-blue">Resultados de laboratorio</h1>
             <p>Medico consultor:</p>
             <p>Fecha de consulta:</p>
 
             <h2>Resultados</h2>
             <section>
-
+                <LabResultsTable
+                    data={data}
+                    options={options} />
             </section>
         </div>
     )
