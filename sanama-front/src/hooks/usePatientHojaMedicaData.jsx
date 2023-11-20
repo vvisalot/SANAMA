24ab5cc67c79f975c6eebe37e551f5291879e9fe
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-20 03:06:03 -0500

    UPDATE

--- sanama-front/src/hooks/usePatientHojaMedicaData.jsx
+++ sanama-front/src/hooks/usePatientHojaMedicaData.jsx
@@ -1,15 +1,33 @@
-"use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { attentionService } from "@/services/attentionService";
 
 const usePatientHojaMedicaData = (idEvaluation) => {
+  const [medicalRecordData, setMedicalRecordData] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [errorMessageMedicalRecordForm, setErrorMessageMedicalRecordForm] =
     useState("");
 
-  return {
-    medicalRecordData,
-    loading,
-    errorMessageMedicalRecordForm,
-  };
+  useEffect(() => {
+    const fetchMedicalRecord = async () => {
+      setLoading(true);
+      try {
+        const data = await attentionService.buscarHojaMedicaPaciente(
+          idEvaluation
+        );
+        setMedicalRecordData(data);
+      } catch (error) {
+        setErrorMessageMedicalRecordForm(
+          "Error fetching medical record: " + error.message
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMedicalRecord();
+  }, [idEvaluation]);
+
+  return { medicalRecordData, loading, errorMessageMedicalRecordForm };
 };
 
 export default usePatientHojaMedicaData;
