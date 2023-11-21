7d191114b29707ec096d007cd404a14372e043fb
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-20 19:39:59 -0500

    n

--- sanama-front/src/components/table/TablePagination.jsx
+++ sanama-front/src/components/table/TablePagination.jsx
@@ -1,68 +1,72 @@
 const TablePagination = ({ table }) => {
-    return (
-        <div className="flex items-center gap-2 mt-2">
-            <button
-                className="border rounded p-1"
-                onClick={() => table.setPageIndex(0)}
-                disabled={!table.getCanPreviousPage()}
-            >
-                {"<<"}
-            </button>
-            <button
-                className="border rounded p-1"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-            >
-                {"<"}
-            </button>
-            <button
-                className="border rounded p-1"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-            >
-                {">"}
-            </button>
-            <button
-                className="border rounded p-1"
-                onClick={() => table.setPageIndex(table.getPageCount() - 1)}
-                disabled={!table.getCanNextPage()}
-            >
-                {">>"}
-            </button>
-            <span className="flex items-center gap-1">
-                <div>Página</div>
-                <strong>
-                    {table.getState().pagination.pageIndex + 1} of{" "}
-                    {table.getPageCount()}
-                </strong>
-            </span>
-            <span className="flex items-center gap-1">
-                | Ir a la página:
-                <input
-                    type="number"
-                    defaultValue={table.getState().pagination.pageIndex + 1}
-                    onChange={(e) => {
-                        const page = e.target.value ? Number(e.target.value) - 1 : 0
-                        table.setPageIndex(page)
-                    }}
-                    className="border p-2 rounded w-16"
-                />
-            </span>
-            <select
-                value={table.getState().pagination.pageSize}
-                onChange={(e) => {
-                    table.setPageSize(Number(e.target.value))
-                }}
-                className="rounded"
-            >
-                {[10, 20, 30, 40].map((pageSize) => (
-                    <option key={pageSize} value={pageSize} >
-                        Mostrar {pageSize}
-                    </option>
-                ))}
-            </select>
-        </div>
-    )
-}
+  return (
+    <div className="inline-flex -space-x-px text-sm mb-2 h-8 gap-4">
+      <div className="inline-flex -space-x-px">
+        <button
+          className="flex items-center justify-center px-3  ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<<"}
+        </button>
+        <button
+          className="flex items-center justify-center px-3  leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {"<"}
+        </button>
+        <button
+          className="flex items-center justify-center px-3  leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {">"}
+        </button>
+        <button
+          className="flex items-center justify-center px-3  leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {">>"}
+        </button>
+      </div>
+      <span className="flex items-center justify-center px-3  ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+        <strong>
+          {table.getState().pagination.pageIndex + 1} de {table.getPageCount()}
+        </strong>
+      </span>
+      <span className="flex items-center gap-1">
+        | Ir a la página:
+        <input
+          type="number"
+          defaultValue={table.getState().pagination.pageIndex + 1}
+          onChange={(e) => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0;
+            table.setPageIndex(page);
+          }}
+          className="border p-2 rounded w-16"
+        />
+      </span>
+      <select
+        value={table.getState().pagination.pageSize}
+        onChange={(e) => {
+          table.setPageSize(Number(e.target.value));
+        }}
+        className="flex items-center gap-1"
+      >
+        {[10, 20, 30, 40].map((pageSize) => (
+          <option
+            key={pageSize}
+            value={pageSize}
+            className="border p-2 rounded w-full"
+          >
+            Mostrar {pageSize}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
-export default TablePagination
+export default TablePagination;
