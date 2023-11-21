d8ac60372821b71b1864b4c6418ffb7e4f0e6fcb
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-21 05:20:33 -0500

    .

--- sanama-front/src/components/Dropdowns/DropdownCheckbox.jsx
+++ sanama-front/src/components/Dropdowns/DropdownCheckbox.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useRef, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHourglass } from "@fortawesome/free-solid-svg-icons";
 
 const DropdownCheckbox = ({
   statusList,
@@ -52,15 +50,14 @@ const DropdownCheckbox = ({
         onClick={toggleDropdown}
         id="dropdownBgHoverButton"
         type="button"
-        className={`${height} truncate text-slate-900 hover:bg-slate-400 w-[180px] justify-between bg-gray-50
+        className={`${height} text-slate-900 hover:bg-slate-400 w-[180px] justify-between bg-gray-50
                             border border-gray-300 focus:ring-4 focus:outline-none focus:ring-blue-300 
                             font-normal rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center 
                             dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
       >
-        {getStatusText()}
+        <div className="truncate flex-shrink">{getStatusText()}</div>
         <svg
-          className="w-2.5 h-2.5 ml-2.5 "
-          aria-hidden="true"
+          className="w-2.5 h-2.5 ml-2.5 flex-shrink-0" // Agregar flex-shrink-0 aquí          aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 10 6"
@@ -74,10 +71,11 @@ const DropdownCheckbox = ({
           />
         </svg>
       </button>
+
       {showDropdown && (
         <div
           id="dropdownBgHover"
-          className="z-10 w-[200px]  bg-white absolute rounded-lg shadow dark:bg-gray-700"
+          className="z-10 w-[180px]  bg-white absolute rounded-lg shadow dark:bg-gray-700"
         >
           <ul
             className="p-2 space-y-1 text-sm text-gray-700 dark:text-gray-200"
