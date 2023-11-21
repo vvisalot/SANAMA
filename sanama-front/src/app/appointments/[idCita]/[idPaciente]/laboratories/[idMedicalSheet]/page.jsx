e9dad2c92777e602ee8dad20e2d5033412d4b74a
ANDRESR4444 <130945498+ANDRESR4444@users.noreply.github.com>
2023-11-21 18:13:54 -0500

    pantalla terminada ahora andare mejorando la visi lidad en otras

--- sanama-front/src/app/appointments/[idCita]/[idPaciente]/laboratories/[idMedicalSheet]/page.jsx
+++ sanama-front/src/app/appointments/[idCita]/[idPaciente]/laboratories/[idMedicalSheet]/page.jsx
@@ -9,13 +9,15 @@ import { parseLaboratoryMSTable } from '@/util/laboratoryMSParser';
 const LaboratoryMSPage = () => {
     
     const [laboratoryData, setLaboratoryData] = useState([]);
-    
+    const [unparsedData, setUnparsedData] = useState([]);
+        
     const idHojaMedica = 30;
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await appointmentService.listarLaboratorios(idHojaMedica);
+                setUnparsedData(data);
                 console.log("LA DATA SIN PARSEAR ES: ", data)
                 const parsedData = parseLaboratoryMSTable(data);
                 console.log("LA DATA ES: ", parsedData)
@@ -31,11 +33,105 @@ const LaboratoryMSPage = () => {
     return (
         <section className="p-4 md:p-14">
             <TitleWithIcon name={"Resultados de Laboratorio"} Icon={LabIcon} />
+            <div className="mb-4 flex flex-grow" style={{ flex: '3 0 0%' }}> 
+              <InputField
+                label="Médico prescriptor"
+                value={unparsedData?.medicoConsulta}
+                disabled
+                width="w-1/2"
+                labelWidth="w-full"
+              />
+            </div>
             <section>
                 <LaboratoryMSTable data={laboratoryData}></LaboratoryMSTable>
             </section>
+
+            <div className="col-span-3">
+                <h4 className="text-3xl font-bold mb-4 mt-4">
+                Observaciones
+                </h4>
+
+                <InputField
+                value={unparsedData?.observaciones}
+                type="textarea" 
+                name="observaciones"
+                maxLength={1000}
+                />       
+            </div>
         </section>
     );
 };
+
+const InputField = ({
+    label,
+    name,
+    value,
+    isEditable,
+    type = "text",
+    onChange,
+    options = [],
+    width = "w-full",
+    labelWidth,
+    placeholder = "Seleccione una opción",
+  }) => {
+    const inputClass = `border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block ${width}  ${
+      isEditable ? 'bg-white cursor-text' : 'bg-gray-300 cursor-not-allowed'
+    }`;
+  
+    const renderInput = () => {
+      switch (type) {
+        case 'textarea':
+          return (
+            <textarea
+              name={name}
+              id={name}
+              value={value}
+              disabled={!isEditable}
+              onChange={onChange}
+              className={`flex-1 ${inputClass}`}
+            />
+          );
+        case 'select':
+          return (
+            <select
+              name={name}
+              id={name}
+              value={value}
+              disabled={!isEditable}
+              onChange={onChange}
+              className={`flex-1 ${inputClass}`}
+            >
+              <option value="" disabled selected>{placeholder}</option> 
+              {options.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+          );
+        default:
+          return (
+            <input
+              type={type}
+              name={name}
+              id={name}
+              value={value}
+              disabled={!isEditable}
+              onChange={onChange}
+              className={`flex-1 ${inputClass}`}
+            />
+          );
+      }
+    };
+  
+    return (
+      <div className={`mb-2 ${width}`}>
+        <label className={`block text-lg font-medium text-gray-700 mb-2 ${labelWidth}`} htmlFor={name}>
+          {label}
+        </label>
+        {renderInput()}
+      </div>
+    );
+  };
 
 export default LaboratoryMSPage;
