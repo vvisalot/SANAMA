0079c60caaf4b1d911b3dfddb32cd104d39f5da3
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-21 06:25:27 -0500

    mejora

--- sanama-front/src/components/navbar/NavbarItem.jsx
+++ sanama-front/src/components/navbar/NavbarItem.jsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-
 const NavbarItem = ({ name, route, isActive, icon: Icon }) => {
   const baseItemClasses =
     "flex items-center transition duration-100 ease-in-out scale-90 hover:scale-100";
@@ -14,8 +13,8 @@ const NavbarItem = ({ name, route, isActive, icon: Icon }) => {
     <li className={itemClasses}>
       <Link href={route} className="w-full h-full">
         <href className="flex items-center px-3 rounded-2xl w-full h-full ">
-          {Icon && <Icon className="mr-2" />} {/* Render the icon here */}
-          <span className="">{name}</span>
+          {Icon && <Icon className="mr-2" width={25} height={25} />}
+          <span className="text-lg font-sans">{name}</span>
         </href>
       </Link>
     </li>
--- sanama-front/src/components/table/TablePagination.jsx
+++ sanama-front/src/components/table/TablePagination.jsx
@@ -39,22 +39,22 @@ const TablePagination = ({ table }) => {
             const page = e.target.value ? Number(e.target.value) - 1 : 0;
             table.setPageIndex(page);
           }}
-          className="w-2/6 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg"
+          className="w-16 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg"
           aria-label="Número de página"
         />
         <select
           value={table.getState().pagination.pageSize}
           onChange={(e) => table.setPageSize(Number(e.target.value))}
-          className="flex items-center justify-center leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center h-8 leading-[12px] text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           aria-label="Elementos por página"
         >
           {[10, 20, 30, 40].map((pageSize) => (
             <option
               key={pageSize}
               value={pageSize}
-              className="items-center justify-center"
+              className="text-center align-middle text-gray-500"
             >
-              Mostrar {pageSize}
+              Ver {pageSize}
             </option>
           ))}
         </select>
