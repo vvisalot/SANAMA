1511970e48a4dde6b0636728d6aed539cb31670b
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-23 01:49:56 -0500

    .

--- sanama-front/src/app/appointments/[idCita]/[idPaciente]/page.jsx
+++ sanama-front/src/app/appointments/[idCita]/[idPaciente]/page.jsx
@@ -170,7 +170,7 @@ const HistorialClinico = () => {
             type="button"
             className={`font-bold py-2 px-4 rounded ${
               isEvaluationButtonDisabled
-                ? "bg-gray-500 hover:bg-gray-600"
+                ? "bg-gray-500"
                 : "bg-green-500 hover:bg-green-700"
             } text-white`}
             onClick={handleNewMedicalEvaluation}
