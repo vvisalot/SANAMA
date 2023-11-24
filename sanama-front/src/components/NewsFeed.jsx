d02f13c4f0595ad5ffd022f4196464932b38f179
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-11-24 17:07:08 -0500

    update casi final

--- sanama-front/src/components/NewsFeed.jsx
+++ sanama-front/src/components/NewsFeed.jsx
@@ -11,6 +11,9 @@ const NewsFeed = () => {
         const response = await fetch(
           `https://newsapi.org/v2/top-headlines?category=health&language=en&apiKey=0b60f29bfedd406996d2930eeeb7411f`
         );
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
         const data = await response.json();
         setNewsList(data.articles);
       } catch (error) {
@@ -29,7 +32,12 @@ const NewsFeed = () => {
 
   return (
     <div className="w-[23.5rem]">
-      <div className="text-xl font-bold text-black-500 px-4" style={{ color: "#28539E" }}>Noticias</div>
+      <div
+        className="text-xl font-bold text-black-500 px-4"
+        style={{ color: "#28539E" }}
+      >
+        Noticias
+      </div>
       <div className="flex flex-col">
         {newsList.slice(1, 6).map((news, index) => (
           <NewsItem key={index} news={news} />
