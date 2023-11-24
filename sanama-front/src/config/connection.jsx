f84b19b4e092137c1244ae6c4104b006301a869a
zaicket <vargas.pedro@pucp.edu.pe>
2023-11-24 11:43:36 -0500

    .

--- sanama-front/src/config/connection.jsx
+++ sanama-front/src/config/connection.jsx
@@ -1,6 +1,6 @@
 const connection = {
-	// backend: "http://3.213.90.160:8080",
-	backend: 'http://localhost:8080'
+	 backend: "http://3.213.29.160:8080",
+	//backend: 'http://localhost:8080'
 }
 
 export default connection
