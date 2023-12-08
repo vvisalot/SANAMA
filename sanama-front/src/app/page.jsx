67387e92c613b87d19dde61b9eb43941fe5c9c26
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-12-08 08:48:58 -0500

    update

--- sanama-front/src/app/page.jsx
+++ sanama-front/src/app/page.jsx
@@ -1,37 +1,37 @@
-//Pagina de inicio al cargar por primera vez la aplicacion
-//TODO: Un componente Dashboard que muestre datos iniciales.
 "use client";
 import React from "react";
 import NewsFeed from "@/components/NewsFeed";
-import DashboardStatsGrid from '../components/dashboard/DashboardStatsGrid'
-import TransactionChart from '../components/dashboard/TransactionChart'
-import BuyerProfilePieChart from '../components/dashboard/BuyerProfilePieChart'
-import CitasRecientes from '../components/dashboard/CitasRecientes'
-import TitleWithIcon from "@/components/TitleWithIcon";
+import DashboardStatsGrid from "../components/dashboard/DashboardStatsGrid";
+import TransactionChart from "../components/dashboard/TransactionChart";
+import BuyerProfilePieChart from "../components/dashboard/BuyerProfilePieChart";
+import CitasRecientes from "../components/dashboard/CitasRecientes";
+
 export default function Home() {
+  const currentDate = new Date();
+  const currentMonth = currentDate.toLocaleString("default", { month: "long" });
+  const currentDay = currentDate.getDate();
+
   return (
-    // <main className="flex flex-col items-center justify-between p-10">
-    //   <div className="font-bold text-blue-500">Noticias Medicas</div>
-    //   <NewsFeed />
-    // </main>
-    <>
-      <TitleWithIcon name={"Inicio"} />
-      <div className="text-5xl font-bold text-black-500 px-4 text-center mb-6" style={{ color: "#28539E" }}>
-        Indicadores - Noviembre 2023
+    <section className="w-full">
+      <div
+        className="text-5xl font-bold text-black-500 px-4 text-center mb-6"
+        style={{ color: "#28539E" }}
+      >
+        Indicadores - {currentMonth} {currentDay}, 2023
       </div>
+
       <div className="flex flex-col gap-4">
         <DashboardStatsGrid />
         <div className="flex flex-row gap-4 w-full">
           <TransactionChart />
           <BuyerProfilePieChart />
         </div>
-        
+
         <div className="flex flex-row gap-4 w-full">
           <CitasRecientes />
           <NewsFeed />
         </div>
       </div>
-      {/* <div className="font-bold text-blue-500">Noticias Medicas</div> */}
-    </>
+    </section>
   );
 }
--- sanama-front/src/components/dashboard/CitasRecientes.jsx
+++ sanama-front/src/components/dashboard/CitasRecientes.jsx
@@ -1,100 +1,50 @@
-'use client'
-import { useEffect, useState, useCallback } from 'react'
-import { appointmentService } from '@/services/appointmentService'
-import { doctorService } from '@/services/doctorService'
-import { useAppointmentData } from '@/hooks/useAppointmentData'
-import AppointmentTable from '@/components/appointments/AppointmentTable'
-import SearchBar from '@/components/bars/SearchBar'
-import DateRangePicker from '@/components/Date/DateRangePicker'
-import DropdownCheckbox from '@/components/Dropdowns/DropdownCheckbox'
-import AppointmentIcon from '@/components/icons/AppointmentIcon'
-import TitleWithIcon from '@/components/TitleWithIcon'
-import Dropdown from '@/components/Dropdowns/Dropdown'
-import { format } from 'date-fns'
-import Link from 'next/link'
+"use client";
+import { useEffect, useState } from "react";
+import { useAppointmentData } from "@/hooks/useAppointmentData";
+import AppointmentTable from "@/components/appointments/AppointmentTable";
 
 const initialRequest = {
-	pn_id_especialidad: null,
-	pv_filtro: '',
-	pd_fecha_inicio: null,
-	pd_fecha_fin: null,
-	arregloEstados: [{
-		"estado": 4
-	}]
-}
+  pn_id_especialidad: null,
+  pv_filtro: "",
+  pd_fecha_inicio: null,
+  pd_fecha_fin: null,
+  arregloEstados: [],
+};
 
-const AppointmentPage = () => {
-	const [specialties, setSpecialties] = useState([])
-	const [statusList, setStatusList] = useState([])
-	const [statusState, setStatusState] = useState({})
-	const [dateInitial, setDateInitial] = useState(null)
-	const [dateFinal, setDateFinal] = useState(null)
-	const [especialidadSeleccionada, setEspecialidadSeleccionada] = useState('Todas las especialidades')
+const CitasRecientes = () => {
+  const [loadingTable, setLoadingTable] = useState(true);
+  const { appointmentTable, fetchData } = useAppointmentData();
 
-	const [loadingTable, setLoadingTable] = useState(true)
-	const { appointmentTable, fetchData } = useAppointmentData()
+  useEffect(() => {
+    fetchData(initialRequest);
+  }, [initialRequest]);
 
-	const fetchInitialData = useCallback(async () => {
-		try {
-			const [statusData, specialtyData] = await Promise.all([appointmentService.listarEstados(), doctorService.listarEspecialidades()])
-			setStatusList(statusData)
-			setSpecialties(specialtyData)
-			const statusStateInit = statusData.reduce((acc, status) => ({ ...acc, [status.idValue]: false }), {})
-			setStatusState(statusStateInit)
-			setLoadingTable(false)
-		} catch (error) {
-			console.error('Error al obtener datos iniciales:', error)
-			setLoadingTable(false)
-		}
-	}, [])
+  const options = [
+    {
+      text: "Ver",
+      link: "/appointments",
+      icon: "/icons/eye.svg",
+    },
+  ];
 
-	useEffect(() => {
-		fetchInitialData()
-		fetchData(initialRequest)
-	}, [fetchInitialData])
+  return (
+    <section className="w-full">
+      <div
+        className="text-xl font-bold text-black-500 px-4"
+        style={{ color: "#28539E" }}
+      >
+        Citas pendientes
+      </div>
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
-		const elements = e.target.elements
-		const filtro = elements.namedItem('search-bar-appointments').value
-		const filtroEspecialidad = elements.namedItem('speciality-dropdown').value
+      <section className="w-full p-4">
+        <AppointmentTable
+          data={appointmentTable}
+          options={options}
+          loadingTable={loadingTable}
+        />
+      </section>
+    </section>
+  );
+};
 
-		const stateArray = Object.entries(statusState)
-			.filter(([key, value]) => value)
-			.map(([key, value]) => {
-				return {
-					estado: key
-				}
-			})
-
-		const request = {
-			pn_id_especialidad: filtroEspecialidad ? filtroEspecialidad : null,
-			pv_filtro: filtro,
-			pd_fecha_inicio: dateInitial ? format(dateInitial, 'yyyy-MM-dd') : null,
-			pd_fecha_fin: dateFinal ? format(dateFinal, 'yyyy-MM-dd') : null,
-			arregloEstados: stateArray
-		}
-		fetchData(request)
-	}
-
-	const options = [
-		{
-			text: 'Ver',
-			link: '/appointments',
-			icon: '/icons/eye.svg'
-		}
-	]
-
-	return (
-		<section className="w-full">
-			
-            <div className="text-xl font-bold text-black-500 px-4" style={{ color: "#28539E" }}>Citas pendientes</div>
-
-			<section className="w-full p-4">
-				<AppointmentTable data={appointmentTable} options={options} loadingTable={loadingTable} />
-			</section>
-		</section>
-	)
-}
-
-export default AppointmentPage
+export default CitasRecientes;
