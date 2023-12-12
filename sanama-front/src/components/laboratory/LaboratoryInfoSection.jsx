c5fbfc9545bdafc9fe155b8b2254c8da0993e9fd
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-12-12 03:40:18 -0500

    update

--- sanama-front/src/components/laboratory/LaboratoryInfoSection.jsx
+++ sanama-front/src/components/laboratory/LaboratoryInfoSection.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import InputField from "../common/InputField";
 import { calcularEdad, formatearFecha } from "@/util/formValidations";
 import { getStatus } from "@/util/status";
 
@@ -19,6 +18,10 @@ const LaboratoryInfoSection = ({ dataLaboratory }) => {
       : "Femenino";
 
   const estado = getStatus(dataLaboratory?.estado);
+  const nombrePaciente =
+    `${dataLaboratory?.citaMedica?.paciente?.nombres} ${dataLaboratory?.citaMedica?.paciente?.apellidoPaterno} ${dataLaboratory?.citaMedica?.paciente?.apellidoMaterno}` ||
+    "";
+  const dni = dataLaboratory?.citaMedica?.paciente?.dni || "";
 
   return (
     <div className="col-span-2">
@@ -27,47 +30,107 @@ const LaboratoryInfoSection = ({ dataLaboratory }) => {
       </h4>
 
       <div className="ml-4 grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-        <InputField
-          label="Nombre completo"
-          value={`${dataLaboratory?.citaMedica?.paciente?.nombres} ${dataLaboratory?.citaMedica?.paciente?.apellidoPaterno} ${dataLaboratory?.citaMedica?.paciente?.apellidoMaterno}`}
-          disabled
-          width="w-full"
-        />
-        <InputField
-          label="Documento de identidad"
-          value={dataLaboratory?.citaMedica?.paciente?.dni}
-          disabled
-          width="w-full"
-        />
-        <InputField
-          label="Fecha de Nacimiento"
-          value={fechaNacimientoFormateada}
-          disabled
-        />
-        <InputField label="Edad" value={edad} disabled width="w-full" />
-        <InputField label="Sexo" value={sexo} disabled width="w-full" />
-        <InputField label="Estado de la Orden" value={estado.text} disabled />
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Nombre completo
+          </label>
+          <input
+            type="text"
+            value={nombrePaciente}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Documento de identidad
+          </label>
+          <input
+            type="text"
+            value={dni}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Fecha de Nacimiento
+          </label>
+          <input
+            type="text"
+            value={fechaNacimientoFormateada}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Edad
+          </label>
+          <input
+            type="text"
+            value={edad}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Sexo
+          </label>
+          <input
+            type="text"
+            value={sexo}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Estado de la Orden
+          </label>
+          <input
+            type="text"
+            value={estado.text}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
       </div>
       <div className="ml-4 grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-        <InputField
-          label="Médico Prescriptor"
-          value={`${dataLaboratory?.citaMedica?.medico?.nombres} ${dataLaboratory?.citaMedica?.medico?.apellidoPaterno} ${dataLaboratory?.citaMedica?.medico?.apellidoMaterno}`}
-          disabled
-          width="w-full"
-        />
-        <InputField
-          label="Tipo de Examen"
-          value={dataLaboratory?.tipoMuestra}
-          disabled
-          width="w-full"
-        />
-        <InputField
-          label="Exámenes a realizar"
-          value={dataLaboratory?.instrucciones}
-          disabled
-          type="textarea"
-          width="w-full"
-        />
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Médico Prescriptor
+          </label>
+          <input
+            type="text"
+            value={`${dataLaboratory?.citaMedica?.medico?.nombres} ${dataLaboratory?.citaMedica?.medico?.apellidoPaterno} ${dataLaboratory?.citaMedica?.medico?.apellidoMaterno}`}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Tipo de Muestra
+          </label>
+          <input
+            type="text"
+            value={dataLaboratory?.tipoMuestra}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div>
+          <label className="block text-sm font-medium text-gray-500">
+            Exámenes a realizar
+          </label>
+          <input
+            type="text"
+            value={dataLaboratory?.instrucciones}
+            disabled
+            className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
       </div>
     </div>
   );
