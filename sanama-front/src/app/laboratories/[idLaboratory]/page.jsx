1f49426f37935866301895ccd819ae1f1b6c4360
Duvet05 <gonzalo.galvezc@pucp.edu.pe>
2023-12-11 20:37:55 -0500

    update lab

--- sanama-front/src/app/laboratories/[idLaboratory]/page.jsx
+++ sanama-front/src/app/laboratories/[idLaboratory]/page.jsx
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import useLaboratoryProfile from "@/hooks/useLaboratoryOrder";
 import ActionButtonsLab from "@/components/laboratory/ActionButtonsLab";
 import LaboratoryInfoSection from "@/components/laboratory/LaboratoryInfoSection";
+import LaboratoryExamInfoSection from "@/components/laboratory/LaboratoryExamInfoSection";
 
 const LaboratoryProfile = ({ params }) => {
   const {
@@ -23,7 +24,6 @@ const LaboratoryProfile = ({ params }) => {
   const [missingFields, setMissingFields] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [showConfirmPopup, setShowConfirmPopup] = useState(false);
-
   const downloadFile = (base64, fileName) => {
     const blob = base64ToBlob(base64, "application/pdf");
 
@@ -37,7 +37,6 @@ const LaboratoryProfile = ({ params }) => {
     document.body.removeChild(link);
     URL.revokeObjectURL(url);
   };
-
   const base64ToBlob = (base64, mimeType) => {
     const byteCharacters = atob(base64);
     const byteNumbers = new Array(byteCharacters.length);
@@ -66,75 +65,6 @@ const LaboratoryProfile = ({ params }) => {
     }
   };
 
-  const handleAddExamenClick = () => {
-    setDataLaboratory((prevState) => {
-      const lastId =
-        prevState.examenMedico.length > 0
-          ? Math.max(...prevState.examenMedico.map((e) => e.idExamen || 0))
-          : 0;
-
-      return {
-        ...prevState,
-        examenMedico: [
-          ...prevState.examenMedico,
-          { idExamen: lastId + 1, nombreArchivo: "", archivo: null },
-        ],
-      };
-    });
-
-    setTimeout(() => {
-      const newExamenIndex = dataLaboratory.examenMedico.length;
-      const newFileInput = document.querySelector(
-        `input[name="examenMedico.${newExamenIndex}.archivo"]`
-      );
-      if (newFileInput) {
-        newFileInput.click();
-      }
-    }, 0);
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    if (name.includes("citaMedica.paciente.")) {
-      const field = name.split(".")[2];
-      setDataLaboratory((prevState) => ({
-        ...prevState,
-        citaMedica: {
-          ...prevState.citaMedica,
-          paciente: {
-            ...prevState.citaMedica.paciente,
-            [field]: value,
-          },
-        },
-      }));
-    } else if (name.includes("citaMedica.medico.")) {
-      const field = name.split(".")[2];
-      setDataLaboratory((prevState) => ({
-        ...prevState,
-        citaMedica: {
-          ...prevState.citaMedica,
-          medico: {
-            ...prevState.citaMedica.medico,
-            [field]: value,
-          },
-        },
-      }));
-    } else {
-      setDataLaboratory((prevState) => ({
-        ...prevState,
-        [name]: value,
-      }));
-    }
-
-    if (name === "instrucciones" || name === "observaciones") {
-      const count = value.length;
-      if (charCountRef.current) {
-        charCountRef.current.textContent = `${count}/1000`;
-      }
-    }
-  };
-
   const handleClosePopup = () => {
     setShowConfirmPopup(false);
   };
@@ -143,77 +73,6 @@ const LaboratoryProfile = ({ params }) => {
     setShowModal(false);
     if (typeof window !== "undefined") {
       window.history.back();
-    }
-  };
-
-  const handleFileChange = (e, index) => {
-    const file = e.target.files[0];
-    if (file && file.type === "application/pdf") {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        if (reader.readyState === 2) {
-          const base64 = reader.result.split(",")[1];
-          setDataLaboratory((prevState) => {
-            const updatedExamenMedico = [...prevState.examenMedico];
-            updatedExamenMedico[index] = {
-              ...updatedExamenMedico[index],
-              nombreArchivo: file.name,
-              archivo: base64,
-            };
-            return {
-              ...prevState,
-              examenMedico: updatedExamenMedico,
-            };
-          });
-        }
-      };
-    } else {
-      console.error("Archivo no permitido o no es un PDF.");
-    }
-  };
-
-  const handleRemoveExamen = (indexToRemove) => {
-    setDataLaboratory((prevState) => ({
-      ...prevState,
-      examenMedico: prevState.examenMedico.filter(
-        (_, index) => index !== indexToRemove
-      ),
-    }));
-  };
-
-  const handleAddExamen = (event) => {
-    const files = event.target.files;
-
-    if (!files.length) {
-      return;
-    }
-
-    const file = files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      setDataLaboratory((prevState) => {
-        const lastId =
-          prevState.examenMedico.length > 0
-            ? Math.max(...prevState.examenMedico.map((e) => e.idExamen || 0))
-            : 0;
-        return {
-          ...prevState,
-          examenMedico: [
-            ...prevState.examenMedico,
-            {
-              idExamen: lastId + 1,
-              nombreArchivo: file.name,
-              archivo: reader.result,
-            },
-          ],
-        };
-      });
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
     }
   };
 
@@ -226,134 +85,6 @@ const LaboratoryProfile = ({ params }) => {
         />
 
         <LaboratoryInfoSection dataLaboratory={dataLaboratory} />
-
-        <div>
-          <div>
-            <h2 className="text-3xl font-bold mt-5">
-              Información de los exámenes
-            </h2>
-          </div>
-
-          <div className="grid grid-cols-3 gap-6 mb-6">
-            <div className="flex justify-start">
-              <div style={{ flex: "0 0 350px", marginRight: "14rem" }}>
-                <input
-                  label="Médico de Laboratorio"
-                  name="medicoLaboratorio"
-                  value={
-                    medicos.find(
-                      (medico) =>
-                        medico.descripcion === dataLaboratory.doctorFirmante
-                    )?.idValue || ""
-                  }
-                  type="select"
-                  onChange={handleMedicoChange}
-                  options={medicos.map((medico) => ({
-                    value: medico.idValue,
-                    label: medico.descripcion,
-                  }))}
-                  isEditable={isEditable}
-                  labelWidth="w-full"
-                  width="w-full"
-                />
-              </div>
-            </div>
-
-            <div className="col-span-3">
-              <table className="min-w-full divide-y divide-gray-200 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <thead className="bg-gray-300">
-                  <tr>
-                    <th className="px-6 py-3 text-left text-lg font-medium text-gray-700 tracking-wider">
-                      Nombre del archivo
-                    </th>
-                    <th className="px-6 py-3 text-right text-lg font-medium text-gray-700 tracking-wider">
-                      Acciones
-                    </th>
-                  </tr>
-                </thead>
-
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {dataLaboratory.examenMedico.map((examen, index) => (
-                    <tr key={index}>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="flex items-center">
-                          {examen.nombreArchivo ? (
-                            <a
-                              href="#"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                downloadFile(
-                                  examen.archivo,
-                                  examen.nombreArchivo
-                                );
-                              }}
-                              className="text-blue-600 hover:underline"
-                            >
-                              {examen.nombreArchivo}
-                            </a>
-                          ) : (
-                            <input
-                              name={`examenMedico.${index}.archivo`}
-                              className="border rounded p-2 text-md"
-                              type="file"
-                              onChange={(e) => handleFileChange(e, index)}
-                            />
-                          )}
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <button
-                          onClick={() =>
-                            downloadFile(examen.archivo, examen.nombreArchivo)
-                          }
-                          className="text-lg text-indigo-600 hover:text-indigo-900"
-                        >
-                          Descargar
-                        </button>
-                        <button
-                          onClick={() => handleRemoveExamen(index)}
-                          className="text-lg text-red-600 hover:text-red-900 ml-4"
-                        >
-                          Eliminar
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-
-              <div className="flex justify-end">
-                <input
-                  type="file"
-                  ref={hiddenFileInput}
-                  style={{ display: "none" }}
-                  onChange={handleAddExamen}
-                />
-                <button
-                  onClick={handleAddExamenClick}
-                  className="bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 mt-4 rounded"
-                >
-                  <i className="fas fa-plus mr-2"></i> Añadir
-                </button>
-              </div>
-            </div>
-
-            <div className="col-span-3">
-              <h2 className="text-3xl font-bold mb-4">Observaciones</h2>
-              <input
-                value={dataLaboratory?.observaciones}
-                isEditable={isEditable}
-                type="textarea"
-                name="observaciones"
-                onChange={handleChange}
-                maxLength={1000}
-              />
-              <span className="text-right block" ref={charCountRef}>
-                {(dataLaboratory?.observaciones || "").length}/1000
-              </span>
-            </div>
-          </div>
-        </div>
 
         <div>
           <div className="grid grid-cols-3">
--- sanama-front/src/components/laboratory/LaboratoryExamInfoSection.jsx
+++ sanama-front/src/components/laboratory/LaboratoryExamInfoSection.jsx
@@ -1,15 +1,114 @@
-import React, { useState } from "react";
-import SearchMedicalSheet from "../evaluations/SearchHojaMedica";
-import { useParams } from "next/navigation";
+import Input from "postcss/lib/input";
+import React from "react";
+import InputField from "../common/InputField";
 
-const LaboratoryExamInfoSection = ({ setMedicalRecordData }) => {
-  const [showModal, setShowModal] = useState(false);
-  const params = useParams();
-  const idPaciente = params.idPaciente;
+const LaboratoryExamInfoSection = ({
+  medicos,
+  dataLaboratory,
+  setDataLaboratory,
+  handleMedicoChange,
+}) => {
+  const handleAddExamenClick = () => {
+    setDataLaboratory((prevState) => {
+      const lastId =
+        prevState.examenMedico.length > 0
+          ? Math.max(...prevState.examenMedico.map((e) => e.idExamen || 0))
+          : 0;
+
+      return {
+        ...prevState,
+        examenMedico: [
+          ...prevState.examenMedico,
+          { idExamen: lastId + 1, nombreArchivo: "", archivo: null },
+        ],
+      };
+    });
+
+    setTimeout(() => {
+      const newExamenIndex = dataLaboratory.examenMedico.length;
+      const newFileInput = document.querySelector(
+        `input[name="examenMedico.${newExamenIndex}.archivo"]`
+      );
+      if (newFileInput) {
+        newFileInput.click();
+      }
+    }, 0);
+  };
+
+  const handleFileChange = (e, index) => {
+    const file = e.target.files[0];
+    if (file && file.type === "application/pdf") {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          const base64 = reader.result.split(",")[1];
+          setDataLaboratory((prevState) => {
+            const updatedExamenMedico = [...prevState.examenMedico];
+            updatedExamenMedico[index] = {
+              ...updatedExamenMedico[index],
+              nombreArchivo: file.name,
+              archivo: base64,
+            };
+            return {
+              ...prevState,
+              examenMedico: updatedExamenMedico,
+            };
+          });
+        }
+      };
+    } else {
+      console.error("Archivo no permitido o no es un PDF.");
+    }
+  };
+
+  const handleRemoveExamen = (indexToRemove) => {
+    setDataLaboratory((prevState) => ({
+      ...prevState,
+      examenMedico: prevState.examenMedico.filter(
+        (_, index) => index !== indexToRemove
+      ),
+    }));
+  };
+
+  const handleAddExamen = (event) => {
+    const files = event.target.files;
+
+    if (!files.length) {
+      return;
+    }
+
+    const file = files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      setDataLaboratory((prevState) => {
+        const lastId =
+          prevState.examenMedico.length > 0
+            ? Math.max(...prevState.examenMedico.map((e) => e.idExamen || 0))
+            : 0;
+        return {
+          ...prevState,
+          examenMedico: [
+            ...prevState.examenMedico,
+            {
+              idExamen: lastId + 1,
+              nombreArchivo: file.name,
+              archivo: reader.result,
+            },
+          ],
+        };
+      });
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
 
   const handleOnBlurChange = (e) => {
     const { name, value } = e.target;
-    setMedicalRecordData((prevData) => {
+    setDataLaboratory((prevData) => {
       const sections = name.split(".");
       if (sections.length === 2) {
         const section = sections[1];
@@ -25,111 +124,127 @@ const LaboratoryExamInfoSection = ({ setMedicalRecordData }) => {
     });
   };
 
-  const [hojaRefencia, setHojaRefencia] = useState(null);
-
-  const addEvaluation = (selectedHoja) => {
-    setHojaRefencia(selectedHoja);
-
-    if (selectedHoja && selectedHoja.idHojaMedica) {
-      setMedicalRecordData((prevData) => ({
-        ...prevData,
-        hojaRefencia: { idHojaReferenciada: selectedHoja.idHojaMedica },
-      }));
-    } else {
-      console.error("Hoja Médica seleccionada no válida o sin ID.");
-    }
-  };
-
-  const removeEvaluation = () => {
-    setHojaRefencia(null);
-    setMedicalRecordData((prevData) => ({
-      ...prevData,
-      hojaRefencia: null,
-    }));
-  };
-
-  const handleOpenModal = () => {
-    setShowModal(true);
-  };
-
-  // Function to close the modal
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
-  const MedicalSheetDetails = () => {
-    if (!hojaRefencia) return null;
-
-    return (
-      <div className="bg-white p-4 rounded-md shadow">
-        <h3 className="font-semibold text-lg">
-          Continuar {hojaRefencia.codigo}
-        </h3>
-        <p>
-          Especialidad: {hojaRefencia.citaMedica.medico.especialidad.nombre}
-        </p>
-        <p>
-          Doctor: {hojaRefencia.citaMedica.medico.nombres}{" "}
-          {hojaRefencia.citaMedica.medico.apellidoPaterno}{" "}
-          {hojaRefencia.citaMedica.medico.apellidoMaterno}
-        </p>
-      </div>
-    );
-  };
-
   return (
-    <div className="my-4 ml-4">
-      <div className="grid grid-cols-1 gap-4">
-        <div className="flex flex-row-reverse">
-          <button
-            type="button"
-            className=" m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 
-        font-medium rounded-lg text-l w-full sm:w-auto px-5 py-3 text-center"
-            onClick={handleOpenModal}
-          >
-            Continuar Evaluación Existente
-          </button>
-          <button
-            type="button"
-            onClick={removeEvaluation}
-            className=" m-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 
-        font-medium rounded-lg text-l w-full sm:w-auto px-5 py-3 text-center"
-          >
-            Limpiar
-          </button>
-        </div>
-        <MedicalSheetDetails />
+    <div>
+      <div>
+        <h2 className="text-3xl font-bold mt-5">Información de los exámenes</h2>
+      </div>
 
-        <textarea
-          label="Antecedentes:"
-          name="evaluacionMedica.antecedentes"
-          className="resize-none mt-1 p-2 w-full border-gray-300 rounded-md"
-          placeholder="Ingresa los antecentes.."
-          rows={4}
-          onBlur={handleOnBlurChange}
-        />
-        <textarea
-          label="Motivo de Consulta:"
-          name="evaluacionMedica.motivoConsulta"
-          className="resize-none mt-1 p-2 w-full border-gray-300 rounded-md"
-          placeholder="Ingresa el motivo.."
-          rows={4}
-          onBlur={handleOnBlurChange}
-        />
-        <textarea
-          label="Observaciones Adicionales:"
-          name="evaluacionMedica.observaciones"
-          className="resize-none mt-1 p-2 w-full border-gray-300 rounded-md"
-          placeholder="Ingresa observación.."
-          rows={4}
-          onBlur={handleOnBlurChange}
-        />
-        <SearchMedicalSheet
-          idPaciente={idPaciente}
-          show={showModal}
-          onClose={handleCloseModal}
-          onSelect={addEvaluation}
-        />
+      <div className="grid grid-cols-3 gap-6 mb-6">
+        <div className="flex justify-start">
+          <div style={{ flex: "0 0 350px", marginRight: "14rem" }}>
+            <InputField
+              label="Médico de Laboratorio"
+              name="medicoLaboratorio"
+              value={
+                medicos.find(
+                  (medico) =>
+                    medico.descripcion === dataLaboratory.doctorFirmante
+                )?.idValue || ""
+              }
+              type="select"
+              onChange={handleMedicoChange}
+              options={medicos.map((medico) => ({
+                value: medico.idValue,
+                label: medico.descripcion,
+              }))}
+              isEditable={isEditable}
+              labelWidth="w-full"
+              width="w-full"
+            />
+          </div>
+        </div>
+
+        <div className="col-span-3">
+          <table className="min-w-full divide-y divide-gray-200 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <thead className="bg-gray-300">
+              <tr>
+                <th className="px-6 py-3 text-left text-lg font-medium text-gray-700 tracking-wider">
+                  Nombre del archivo
+                </th>
+                <th className="px-6 py-3 text-right text-lg font-medium text-gray-700 tracking-wider">
+                  Acciones
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="bg-white divide-y divide-gray-200">
+              {dataLaboratory.examenMedico.map((examen, index) => (
+                <tr key={index}>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="flex items-center">
+                      {examen.nombreArchivo ? (
+                        <a
+                          href="#"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            downloadFile(examen.archivo, examen.nombreArchivo);
+                          }}
+                          className="text-blue-600 hover:underline"
+                        >
+                          {examen.nombreArchivo}
+                        </a>
+                      ) : (
+                        <input
+                          name={`examenMedico.${index}.archivo`}
+                          className="border rounded p-2 text-md"
+                          type="file"
+                          onChange={(e) => handleFileChange(e, index)}
+                        />
+                      )}
+                    </div>
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <button
+                      onClick={() =>
+                        downloadFile(examen.archivo, examen.nombreArchivo)
+                      }
+                      className="text-lg text-indigo-600 hover:text-indigo-900"
+                    >
+                      Descargar
+                    </button>
+                    <button
+                      onClick={() => handleRemoveExamen(index)}
+                      className="text-lg text-red-600 hover:text-red-900 ml-4"
+                    >
+                      Eliminar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="flex justify-end">
+            <input
+              type="file"
+              ref={hiddenFileInput}
+              style={{ display: "none" }}
+              onChange={handleAddExamen}
+            />
+            <button
+              onClick={handleAddExamenClick}
+              className="bg-blue-500 text-white hover:bg-blue-600 px-4 py-2 mt-4 rounded"
+            >
+              <i className="fas fa-plus mr-2"></i> Añadir
+            </button>
+          </div>
+        </div>
+
+        <div className="col-span-3">
+          <h2 className="text-3xl font-bold mb-4">Observaciones</h2>
+          <textarea
+            label="Observaciones"
+            name="dataLaboratory.observaciones"
+            className="resize-none mt-1 p-2 w-full border-gray-300 rounded-md"
+            placeholder="Ingresa observación.."
+            rows={4}
+            onBlur={handleOnBlurChange}
+          />
+          <span className="text-right block" ref={charCountRef}>
+            {(dataLaboratory?.observaciones || "").length}/1000
+          </span>
+        </div>
       </div>
     </div>
   );
